6d0740fae7485fee0047442adef6ec8cb48dd692
Kai Dührkop <kai.duehrkop@uni-jena.de>
2013-05-21 17:48:11 +0200

    add ion to ftgraph

--- src/main/java/de/unijena/bioinf/ChemistryBase/ms/ft/FTGraph.java
+++ src/main/java/de/unijena/bioinf/ChemistryBase/ms/ft/FTGraph.java
@@ -1,9 +1,12 @@
 package de.unijena.bioinf.ChemistryBase.ms.ft;
 
 
+import de.unijena.bioinf.ChemistryBase.chem.Ionization;
+
 public interface FTGraph<T extends FTFragment> {
 
     public T getRoot();
+    public Ionization getIonization();
 
 
 
