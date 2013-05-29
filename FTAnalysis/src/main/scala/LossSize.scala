b1bab17e146da920fdea0f00648f59ff92afa441
Kai Dührkop <kai.duehrkop@uni-jena.de>
2013-05-29 13:52:45 +0200

    minors

--- FTAnalysis/src/main/scala/LossSize.scala
+++ FTAnalysis/src/main/scala/LossSize.scala
@@ -10,6 +10,8 @@ class LossSize(val root:String) {
 
   def lossPath = new File(root, "correctLosses.csv")
 
+  val SMALL = 40
+
   val commonLosses = {
     val map = mutable.Map[MolecularFormula, Int]()
     val csvr = new CSVReader(new FileReader(lossPath))
@@ -56,6 +58,27 @@ class LossSize(val root:String) {
     val minimum:Double = xs.minBy(f=>f.loss.getMass).loss.getMass
     val a:Double = n.toDouble / (xs.foldLeft(0d)((v, o) => v + o.count * log(o.loss.getMass/minimum)))
     new ParetoDistribution(a, minimum)
+  }
+
+  def meanSquare(xs:Seq[Double], ys:Seq[Double]) = {
+    val x_ = xs.foldLeft(0d)(_+_)/xs.size
+    val y_ = ys.foldLeft(0d)(_+_)/ys.size
+    val b = (0 to xs.size).foldLeft(0d)((s,i)=>s+(xs(i) - x_)*(ys(i) - y_)) / xs.foldLeft(0d)((s,x)=>s+(x-x_)*(x-x_))
+    (y_, b)
+  }
+
+  def learnLossSizeDistribution(xs:Traversable[LossObservation]):ParetoLossSizeDistribution = {
+    val pareto = learnPareto(xs.filter(x=>x.loss.getMass>=SMALL))
+    // binne Massen in 25 Dalton Paare für Least-Square
+    val bin0 = xs.filter(x=>x.loss.getMass<20)
+    val bin1 = xs.filter(x=>x.loss.getMass<SMALL && x.loss.getMass >= 20)
+    val x0 = 12.5
+    val x1 = 23.5
+    val y0 = bin0.foldLeft(0)((s,x)=> s + x.count)/n.toDouble
+    val y1 = bin1.foldLeft(0)((s,x)=> s + x.count)/n.toDouble
+    val a=(y1-y0)/(x1-x0)
+    val b = y1-a*x1
+    ParetoLossSizeDistribution(pareto.getK, SMALL, a, b)
   }
 
 
