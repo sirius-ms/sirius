489c868e93029d8c198c6bef05d60f661d0c641d
Kai Dührkop <kai.duehrkop@uni-jena.de>
2013-05-31 18:13:10 +0200

    analysis

---
+++ FragmentationTreeConstruction/src/main/java/de/unijena/bioinf/FragmentationTreeConstruction/computation/scoring/TreeSizeScorer.java
@@ -0,0 +1,15 @@
+package de.unijena.bioinf.FragmentationTreeConstruction.computation.scoring;
+
+import de.unijena.bioinf.FragmentationTreeConstruction.model.ProcessedInput;
+import de.unijena.bioinf.FragmentationTreeConstruction.model.ProcessedPeak;
+
+import java.util.List;
+
+public class TreeSizeScorer implements PeakScorer {
+
+
+    @Override
+    public void score(List<ProcessedPeak> peaks, ProcessedInput input, double[] scores) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
