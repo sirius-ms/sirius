69109ad5d60f394efd6198a2297de0ce242c6c53
marcusL <m.ludwig@uni-jena.de>
2013-06-25 12:21:03 +0200

    new EI Ionization

---
+++ src/main/java/de/unijena/bioinf/ChemistryBase/chem/ElectronIonization.java
@@ -0,0 +1,27 @@
+package de.unijena.bioinf.ChemistryBase.chem;
+
+/**
+ * EI Ionization mode. An electron is dislodged from the neutral molecule.
+ * The getMass() is negative to make addToMass() and subtractFromMass() work as indicated.
+ */
+public class ElectronIonization extends Ionization {
+
+
+    public ElectronIonization(){
+    }
+
+    @Override
+    public double getMass() {
+        return -Static.ELECTRON_MASS;
+    }
+
+    @Override
+    public int getCharge() {
+        return 1;
+    }
+
+    @Override
+    public String getFormula() {
+        return "M+.";
+    }
+}
