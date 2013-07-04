a1d498fa06405a8e18b986695984fea083b8b9b1
kai <kai.duehrkop@uni-jena.de>
2013-07-04 11:56:21 +0200

    minors

--- src/main/java/de/unijena/bioinf/babelms/json/JSONDocumentType.java
+++ src/main/java/de/unijena/bioinf/babelms/json/JSONDocumentType.java
@@ -1,7 +1,7 @@
 package de.unijena.bioinf.babelms.json;
 
-import de.unijena.bioinf.ChemistryBase.data.DataDocument;
 import de.unijena.bioinf.ChemistryBase.algorithm.ParameterHelper;
+import de.unijena.bioinf.ChemistryBase.data.DataDocument;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
