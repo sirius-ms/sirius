88d05dfc921e89be69b1c13f28836878a742cf82
kai <kai.duehrkop@uni-jena.de>
2013-09-17 12:14:10 +0200

    minors

--- IsotopePatternAnalysis/src/main/java/de/unijena/bioinf/IsotopePatternAnalysis/IsotopePattern.java
+++ IsotopePatternAnalysis/src/main/java/de/unijena/bioinf/IsotopePatternAnalysis/IsotopePattern.java
@@ -12,6 +12,7 @@ public class IsotopePattern {
 
     private final SimpleSpectrum pattern;
     private final List<ScoredMolecularFormula> candidates;
+    private final double bestScore;
 
     public IsotopePattern(Spectrum<Peak> pattern) {
         this(pattern, Collections.<ScoredMolecularFormula>emptyList());
@@ -20,6 +21,13 @@ public class IsotopePattern {
     public IsotopePattern(Spectrum<Peak> pattern, List<ScoredMolecularFormula> candidates) {
         this.pattern = new SimpleSpectrum(pattern);
         this.candidates = candidates;
+        double bestScore = Double.NEGATIVE_INFINITY;
+        for (ScoredMolecularFormula f : candidates) bestScore = Math.max(f.getScore(), bestScore);
+        this.bestScore = bestScore;
+    }
+
+    public double getBestScore() {
+        return bestScore;
     }
 
     public List<ScoredMolecularFormula> getCandidates() {
