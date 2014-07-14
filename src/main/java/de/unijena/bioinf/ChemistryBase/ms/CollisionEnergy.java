71dd256afd2b5d9e0a7e8e060ffe65d41dea8169
Kai Dührkop <git@k-du.de>
2014-07-15 00:12:09 +0200

    Completely rewritten Fragmentation Tree and Graph Structures.

--- src/main/java/de/unijena/bioinf/ChemistryBase/ms/CollisionEnergy.java
+++ src/main/java/de/unijena/bioinf/ChemistryBase/ms/CollisionEnergy.java
@@ -12,6 +12,42 @@ public class CollisionEnergy {
         this.maxEnergy = max;
     }
 
+    public static CollisionEnergy mergeAll(CollisionEnergy... others) {
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+        for (int k = 0; k < others.length; ++k) {
+            min = Math.min(min, others[k].minEnergy);
+            max = Math.max(max, others[k].maxEnergy);
+        }
+        return new CollisionEnergy(min, max);
+    }
+
+    public static CollisionEnergy fromString(String value) {
+        final int k = value.indexOf('-');
+        if (k >= 0) {
+            final double x = Double.parseDouble(value.substring(0, k));
+            final double y = Double.parseDouble(value.substring(k + 1));
+            return new CollisionEnergy(x, y);
+        } else {
+            final double x = Double.parseDouble(value);
+            return new CollisionEnergy(x, x);
+        }
+    }
+
+    private static String stringify(double minEnergy) {
+        if (Math.abs((int) minEnergy - minEnergy) < 1e-12) return String.valueOf((int) minEnergy);
+        return String.valueOf(minEnergy);
+    }
+
+    public static Comparator<CollisionEnergy> getMinEnergyComparator() {
+        return new Comparator<CollisionEnergy>() {
+            @Override
+            public int compare(CollisionEnergy o1, CollisionEnergy o2) {
+                return Double.compare(o1.minEnergy, o2.minEnergy);
+            }
+        };
+    }
+
     public boolean isOverlapping(CollisionEnergy other) {
         return minEnergy <= other.maxEnergy && maxEnergy >= other.minEnergy;
     }
@@ -27,6 +63,7 @@ public class CollisionEnergy {
     public boolean lowerThan(CollisionEnergy o) {
         return maxEnergy < o.minEnergy;
     }
+
     public boolean greaterThan(CollisionEnergy o) {
         return minEnergy > o.maxEnergy;
     }
@@ -37,33 +74,19 @@ public class CollisionEnergy {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof CollisionEnergy) return equals((CollisionEnergy)obj);
+        if (obj instanceof CollisionEnergy) return equals((CollisionEnergy) obj);
         return false;
     }
 
     public boolean equals(CollisionEnergy obj) {
         if (this == obj) return true;
-        return Math.abs(minEnergy-obj.minEnergy) < 1e-12 && Math.abs(maxEnergy-obj.maxEnergy) < 1e-12;
+        return Math.abs(minEnergy - obj.minEnergy) < 1e-12 && Math.abs(maxEnergy - obj.maxEnergy) < 1e-12;
     }
 
     @Override
     public String toString() {
         if (minEnergy == maxEnergy) return stringify(minEnergy);
         return stringify(minEnergy) + " - " + stringify(maxEnergy);
-    }
-
-    private static String stringify(double minEnergy) {
-        if (Math.abs((int) minEnergy - minEnergy) < 1e-12) return String.valueOf((int)minEnergy);
-        return String.valueOf(minEnergy);
-    }
-
-    public static Comparator<CollisionEnergy> getMinEnergyComparator() {
-        return new Comparator<CollisionEnergy>() {
-            @Override
-            public int compare(CollisionEnergy o1, CollisionEnergy o2) {
-                return Double.compare(o1.minEnergy, o2.minEnergy);
-            }
-        };
     }
 
     @Override
