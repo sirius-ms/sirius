9479c4bdc589e3e3cf6d1a5bfa0054641dc29b1c
xentrics <bastian.seelbinder@uni-jena.de>
2015-03-05 13:33:28 +0100

    glpk dependencies added pushed some files

---
+++ FragmentationTreeConstruction/src/main/java/de/unijena/bioinf/FragmentationTreeConstruction/computation/tree/ilp/GLPKSolver.java
@@ -0,0 +1,9 @@
+package de.unijena.bioinf.FragmentationTreeConstruction.computation.tree.ilp;
+import org.gnu.glpk.GLPK;
+
+/**
+ * Created by xentrics on 04.03.15.
+ */
+public class GLPKSolver {
+
+}
---
+++ FragmentationTreeConstruction/src/main/java/de/unijena/bioinf/FragmentationTreeConstruction/computation/tree/ilp/glpk_test_main.java
@@ -0,0 +1,13 @@
+package de.unijena.bioinf.FragmentationTreeConstruction.computation.tree.ilp;
+import org.gnu.glpk.GLPK;
+
+/**
+ * Created by xentrics on 04.03.15.
+ */
+public class glpk_test_main {
+
+    public static void main(String[] args) {
+
+        System.out.println( GLPK.glp_version());
+    }
+}
