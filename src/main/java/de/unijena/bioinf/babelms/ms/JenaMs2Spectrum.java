2180a86985af87d3977d04eeed4aa941ad6383d9
Kai Dührkop <git@k-du.de>
2015-04-23 11:07:51 +0200

    make it compilable

--- src/main/java/de/unijena/bioinf/babelms/ms/JenaMs2Spectrum.java
+++ src/main/java/de/unijena/bioinf/babelms/ms/JenaMs2Spectrum.java
@@ -1,5 +1,7 @@
 package de.unijena.bioinf.babelms.ms;
 
+import de.unijena.bioinf.ChemistryBase.chem.Ionization;
+import de.unijena.bioinf.ChemistryBase.chem.PeriodicTable;
 import de.unijena.bioinf.ChemistryBase.ms.CollisionEnergy;
 import de.unijena.bioinf.ChemistryBase.ms.Ms2Spectrum;
 import de.unijena.bioinf.ChemistryBase.ms.Peak;
@@ -25,5 +27,15 @@ public class JenaMs2Spectrum extends JenaMsSpectrum implements Ms2Spectrum<Peak>
     @Override
     public CollisionEnergy getCollisionEnergy() {
         return collisionEnergy;
+    }
+
+    @Override
+    public Ionization getIonization() {
+        return PeriodicTable.getInstance().ionByName("[M+H]+");
+    }
+
+    @Override
+    public int getMsLevel() {
+        return 2;
     }
 }
