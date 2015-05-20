b0af36a72f618acf04e2941262000c1862165a99
Kai Dührkop <kai.duehrkop@uni-jena.de>
2015-05-20 14:17:14 +0200

    licences

--- siriusCLI/src/main/java/de/unijena/bioinf/sirius/Sirius.java
+++ siriusCLI/src/main/java/de/unijena/bioinf/sirius/Sirius.java
@@ -22,13 +22,12 @@ import de.unijena.bioinf.ChemistryBase.chem.utils.ScoredMolecularFormula;
 import de.unijena.bioinf.ChemistryBase.ms.*;
 import de.unijena.bioinf.ChemistryBase.ms.ft.FTree;
 import de.unijena.bioinf.ChemistryBase.ms.ft.TreeScoring;
-import de.unijena.bioinf.ChemistryBase.ms.utils.SimpleMutableSpectrum;
-import de.unijena.bioinf.ChemistryBase.ms.utils.SimpleSpectrum;
 import de.unijena.bioinf.ChemistryBase.ms.utils.Spectrums;
 import de.unijena.bioinf.FragmentationTreeConstruction.computation.FragmentationPatternAnalysis;
 import de.unijena.bioinf.FragmentationTreeConstruction.computation.MultipleTreeComputation;
 import de.unijena.bioinf.FragmentationTreeConstruction.computation.TreeIterator;
 import de.unijena.bioinf.FragmentationTreeConstruction.computation.scoring.TreeSizeScorer;
+import de.unijena.bioinf.FragmentationTreeConstruction.computation.tree.ilp.GurobiJniSolver;
 import de.unijena.bioinf.FragmentationTreeConstruction.model.DecompositionList;
 import de.unijena.bioinf.FragmentationTreeConstruction.model.Ms2SpectrumImpl;
 import de.unijena.bioinf.FragmentationTreeConstruction.model.ProcessedInput;
@@ -104,6 +103,9 @@ public class Sirius {
         profile.fragmentationPatternAnalysis.setDefaultProfile(new MutableMeasurementProfile(profile.fragmentationPatternAnalysis.getDefaultProfile()));
         profile.isotopePatternAnalysis.setDefaultProfile(new MutableMeasurementProfile(profile.isotopePatternAnalysis.getDefaultProfile()));
         this.elementPrediction = new ElementPrediction(profile.isotopePatternAnalysis);
+
+        this.profile.fragmentationPatternAnalysis.setTreeBuilder(new GurobiJniSolver());
+
     }
 
     /**
