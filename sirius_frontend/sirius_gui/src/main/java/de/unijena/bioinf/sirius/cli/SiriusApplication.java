9177d21f791a0fada48d43f075bb0b89f445986b
fleisch <markus.fleischauer@uni-jena.de>
2016-06-21 17:41:53 +0200

    linux32 build done, auto manual and readme build done

--- sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/cli/SiriusApplication.java
+++ sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/cli/SiriusApplication.java
@@ -5,26 +5,14 @@ package de.unijena.bioinf.sirius.cli;
  * 15.06.16.
  */
 
-import de.unijena.bioinf.FragmentationTreeConstruction.computation.tree.ilp.GurobiSolver;
 import de.unijena.bioinf.sirius.gui.mainframe.MainFrame;
-import gurobi.GRBEnv;
-import gurobi.GRBException;
-import org.gnu.glpk.GLPK;
 
 /**
  * @author Markus Fleischauer (markus.fleischauer@gmail.com)
  */
 public class SiriusApplication {
 
-    public static void main(String[] args) throws GRBException {
-        //todo remove debugging
-        System.out.println("check gurobi");
-        GurobiSolver s = new GurobiSolver();
-        GRBEnv e = s.getEnv();
-        System.out.println("check GLPK");
-        System.out.println(GLPK.glp_version());
-        e.resetParams();
-
+    public static void main(String[] args) {
 
         final CLI<SiriusGUIOptions> cli = new CLI<>();
         cli.parseArgs(args, SiriusGUIOptions.class);
