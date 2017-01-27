6cc8a78d64a050585b8b7d89084f947975364132
fleisch <markus.fleischauer@uni-jena.de>
2017-01-27 18:51:03 +0100

    spec view bug ich alle experiments ge tdeleted fixed

--- sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/mainframe/results/SpectraVisualizationPanel.java
+++ sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/mainframe/results/SpectraVisualizationPanel.java
@@ -20,32 +20,31 @@ import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.List;
 
-public class SpectraVisualizationPanel extends JPanel implements ActionListener, MSViewerPanelListener, MouseListener, ActiveResultChangedListener{
+public class SpectraVisualizationPanel extends JPanel implements ActionListener, MSViewerPanelListener, MouseListener, ActiveResultChangedListener {
 
 	private JComboBox<String> spectraSelection;
 	private MSViewerPanel msviewer;
-	
+
 	private ExperimentContainer ec;
 	private DefaultComboBoxModel<String> cbModel;
-	
+
 	private DecimalFormat cEFormat;
-	
+
 	private ExperimentContainerDataModel model;
-	
+
 	private JButton zoomIn, zoomOut;
-	
+
 	private boolean zoomed;
-	
+
 	private SiriusResultElement sre;
-	
+
 	JPopupMenu zoomPopMenu;
 	JMenuItem zoomInMI, zoomOutMI;
-	
+
 	public SpectraVisualizationPanel(ExperimentContainer ec) {
 		this.ec = ec;
-		
 		this.sre = null;
-		
+
 		zoomIn = Buttons.getZoomInButton24();
 		zoomOut = Buttons.getZoomOutButton24();
 		zoomIn.addActionListener(this);
@@ -53,9 +52,9 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 		zoomIn.setEnabled(false);
 		zoomOut.setEnabled(false);
 		zoomed = false;
-		
+
 		constructZoomPopupMenu();
-		
+
 		this.cEFormat = new DecimalFormat("#0.0");
 		this.setLayout(new BorderLayout());
 
@@ -63,111 +62,111 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 		JToolBar northPanel = new JToolBar();
 		northPanel.setFloatable(false);
 		cbModel = new DefaultComboBoxModel<>();
-		updateLogic();
-		
+
 		spectraSelection = new JComboBox<String>(cbModel);
-		spectraSelection.addActionListener(this);
 		spectraSelection.setToolTipText("select spectrum");
 
+		updateLogic();
+
 		JLabel l = new JLabel("Spectrum:");
-		l.setBorder(BorderFactory.createEmptyBorder(0,10,0,5));
+		l.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 5));
 		northPanel.add(l);
 		northPanel.add(spectraSelection);
 
-		northPanel.addSeparator(new Dimension(10,10));
+		northPanel.addSeparator(new Dimension(10, 10));
 		northPanel.add(zoomIn);
 		northPanel.add(zoomOut);
 
 		this.add(northPanel, BorderLayout.NORTH);
-		
+
 		model = new ExperimentContainerDataModel(ec);
-		
+
 		msviewer = new MSViewerPanel();
 		msviewer.showPeakInfoOnlyForImportantPeaks(true);
 		msviewer.setData(model);
 		this.add(msviewer);
 		msviewer.addMSViewerPanelListener(this);
 		msviewer.addMouseListener(this);
-		
-//		msviewer.set
 	}
-	
-	public void constructZoomPopupMenu(){
+
+	public void constructZoomPopupMenu() {
 		zoomPopMenu = new JPopupMenu();
 		zoomInMI = new JMenuItem("Zoom in", Icons.Zoom_In_16);
-		zoomOutMI = new JMenuItem("Zoom out",Icons.Zoom_Out_16);
-		
+		zoomOutMI = new JMenuItem("Zoom out", Icons.Zoom_Out_16);
+
 		zoomInMI.addActionListener(this);
 		zoomOutMI.addActionListener(this);
-		
+
 		zoomInMI.setEnabled(false);
 		zoomOutMI.setEnabled(false);
-		
+
 		zoomPopMenu.add(zoomInMI);
 		zoomPopMenu.add(zoomOutMI);
 	}
 
 	private final String MS1_DISPLAY = "MS 1", MSMS_DISPLAY = "MSMS merged";
 
-	private void updateLogic(){
+	private void updateLogic() {
 		HashSet<String> nameStorage = new HashSet<>();
-		boolean hasMs1=false, hasMs2=false, hasMerged=false;
-		if(spectraSelection!=null) spectraSelection.removeActionListener(this);
+		boolean hasMs1 = false, hasMs2 = false, hasMerged = false;
+		spectraSelection.removeActionListener(this);
+
 		cbModel.removeAllElements();
-		if(ec==null){
+		if (ec == null) {
 			this.zoomIn.setEnabled(false);
 			this.zoomOut.setEnabled(false);
 			this.zoomInMI.setEnabled(false);
 			this.zoomOutMI.setEnabled(false);
-		}else{
+			spectraSelection.setEnabled(false);
+		} else {
 			this.zoomIn.setEnabled(false);
 			this.zoomOut.setEnabled(false);
 			this.zoomInMI.setEnabled(false);
 			this.zoomOutMI.setEnabled(false);
 			java.util.List<CompactSpectrum> ms1 = ec.getMs1Spectra();
 			java.util.List<CompactSpectrum> ms2 = ec.getMs2Spectra();
-			if(!(ms1==null||ms1.isEmpty())){
+			if (!(ms1 == null || ms1.isEmpty())) {
 				cbModel.addElement(MS1_DISPLAY);
-				hasMs1=true;
+				hasMs1 = true;
 			}
-			if(ms2!=null){
-				if (ms2.size()>1) {
+			if (ms2 != null) {
+				if (ms2.size() > 1) {
 					cbModel.addElement(MSMS_DISPLAY);
-					hasMerged=true;
+					hasMerged = true;
 				}
-				hasMs2=true;
-				for(CompactSpectrum sp : ms2){
-					
+				hasMs2 = true;
+				for (CompactSpectrum sp : ms2) {
+
 					String value = null;
-					
-					if(sp.getCollisionEnergy()!=null){
+
+					if (sp.getCollisionEnergy() != null) {
 						double minEn = sp.getCollisionEnergy().getMinEnergy();
 						double maxEn = sp.getCollisionEnergy().getMaxEnergy();
-						
-						if(minEn==maxEn){
-							value = cEFormat.format(minEn)+" eV";
-						}else{
-							value = cEFormat.format(minEn)+"-"+cEFormat.format(maxEn)+" eV";
+
+						if (minEn == maxEn) {
+							value = cEFormat.format(minEn) + " eV";
+						} else {
+							value = cEFormat.format(minEn) + "-" + cEFormat.format(maxEn) + " eV";
 						}
 						int counter = 2;
-						while(nameStorage.contains(value)){
-							if(minEn==maxEn){
-								value = cEFormat.format(minEn)+" eV ("+counter+")";
-							}else{
-								value = cEFormat.format(minEn)+"-"+cEFormat.format(maxEn)+" eV ("+counter+")";
+						while (nameStorage.contains(value)) {
+							if (minEn == maxEn) {
+								value = cEFormat.format(minEn) + " eV (" + counter + ")";
+							} else {
+								value = cEFormat.format(minEn) + "-" + cEFormat.format(maxEn) + " eV (" + counter + ")";
 							}
 							counter++;
 						}
-					}else{
+					} else {
 						value = "MSMS";
 						int counter = 2;
-						while(nameStorage.contains(value)){
-							value = "MSMS ("+counter+")";
+						while (nameStorage.contains(value)) {
+							value = "MSMS (" + counter + ")";
 							counter++;
 						}
 					}
-					
-					
+
+
 					nameStorage.add(value);
 					cbModel.addElement(value);
 				}
@@ -175,7 +174,7 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 
 			final ExperimentContainerDataModel.DisplayMode currentMode = model.getMode();
 			final int currentIndex = model.getSelectedMs2Spectrum();
-			if(cbModel.getSize()>0) {
+			if (cbModel.getSize() > 0) {
 				if (currentMode == ExperimentContainerDataModel.DisplayMode.MS) {
 					if (hasMs1) cbModel.setSelectedItem(MS1_DISPLAY);
 					else spectraSelection.setSelectedIndex(0);
@@ -184,47 +183,55 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 					int i = currentIndex;
 					if (hasMs1) ++i;
 					if (hasMerged) ++i;
-					i = Math.max(0,Math.min(i, spectraSelection.getItemCount()-1));
+					i = Math.max(0, Math.min(i, spectraSelection.getItemCount() - 1));
 					spectraSelection.setSelectedIndex(i);
 				} else if (currentMode == ExperimentContainerDataModel.DisplayMode.MERGED) {
 					if (hasMerged) cbModel.setSelectedItem(MSMS_DISPLAY);
 					else spectraSelection.setSelectedIndex(hasMs1 && hasMs2 ? 1 : 0);
 				} else {
-					if(cbModel.getSize()>0) spectraSelection.setSelectedIndex(0);
+					if (cbModel.getSize() > 0) spectraSelection.setSelectedIndex(0);
 					else spectraSelection.setSelectedIndex(-1);
 				}
 			} else spectraSelection.setSelectedIndex(-1);
-		}
-		
-		if(spectraSelection!=null){
+
 			spectraSelection.setEnabled(true);
 			spectraSelection.addActionListener(this);
 		}
 	}
 
 	public void resultsChanged(ExperimentContainer ec, SiriusResultElement sre) {
-		this.ec = ec;
-		this.sre = sre;
-		updateLogic();
-		this.model.changeData(ec, sre, getDisplayMode(), getSelectedMs2Spectrum());
-		msviewer.setData(model);
-		msviewer.repaint();
+		if (this.ec != ec || this.sre != sre) {
+			System.out.println("update Spec");
+			this.ec = ec;
+			this.sre = sre;
+
+			updateLogic();
+
+			if (ec != null) {
+				this.model.changeData(ec, sre, getDisplayMode(), getSelectedMs2Spectrum());
+			} else {
+				this.model = new ExperimentContainerDataModel(null);
+			}
+
+			msviewer.setData(model);
+			msviewer.repaint();
+		}
 	}
-	
-	public ExperimentContainer getExperiment(){
+
+	public ExperimentContainer getExperiment() {
 		return ec;
 	}
 
 	private int getSelectedMs2Spectrum() {
 		int index = this.spectraSelection.getSelectedIndex();
-		if (ec.getMs1Spectra().size()>0) --index;
-		if (ec.getMs2Spectra().size()>1) --index;
+		if (ec.getMs1Spectra().size() > 0) --index;
+		if (ec.getMs2Spectra().size() > 1) --index;
 		return index;
 	}
 
 	private ExperimentContainerDataModel.DisplayMode getDisplayMode() {
-		String s = (String)this.spectraSelection.getSelectedItem();
-		if (s==null) return ExperimentContainerDataModel.DisplayMode.DUMMY;
+		String s = (String) this.spectraSelection.getSelectedItem();
+		if (s == null) return ExperimentContainerDataModel.DisplayMode.DUMMY;
 		if (s.equals(MS1_DISPLAY)) return ExperimentContainerDataModel.DisplayMode.MS;
 		else if (s.equals(MSMS_DISPLAY)) return ExperimentContainerDataModel.DisplayMode.MERGED;
 		else return ExperimentContainerDataModel.DisplayMode.MSMS;
@@ -232,11 +239,11 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==spectraSelection){
+		if (e.getSource() == spectraSelection) {
 			java.util.List<CompactSpectrum> ms1 = ec.getMs1Spectra();
 			java.util.List<CompactSpectrum> ms2 = ec.getMs2Spectra();
 			int index = this.spectraSelection.getSelectedIndex();
-			if(index<0) return;
+			if (index < 0) return;
 			final ExperimentContainerDataModel.DisplayMode mode = getDisplayMode();
 			if (mode == ExperimentContainerDataModel.DisplayMode.MS) {
 				model.selectMS1Spectrum();
@@ -250,19 +257,19 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 			msviewer.setData(model);
 			this.showMolecularFormulaMarkings();
 //			msviewer.repaint();
-		}else if(e.getSource()==zoomIn || e.getSource()==zoomInMI){
+		} else if (e.getSource() == zoomIn || e.getSource() == zoomInMI) {
 			zoomIn.setEnabled(false);
 			zoomInMI.setEnabled(false);
 			int start = model.getFirstMarkedIndex();
 			int end = model.getLastMarkedIndex();
 			this.model.removeMarkings();
-			if(start<0||end<0||start==end){
+			if (start < 0 || end < 0 || start == end) {
 				return;
 			}
 			this.msviewer.showZoomedView(start, end);
 			zoomOut.setEnabled(true);
 			zoomOutMI.setEnabled(true);
-		}else if(e.getSource()==zoomOut || e.getSource()==zoomOutMI){
+		} else if (e.getSource() == zoomOut || e.getSource() == zoomOutMI) {
 			zoomOut.setEnabled(false);
 			zoomOutMI.setEnabled(false);
 			this.model.removeMarkings();
@@ -270,10 +277,10 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 			zoomIn.setEnabled(true);
 			zoomInMI.setEnabled(true);
 		}
-		
+
 	}
-	
-	private void showMolecularFormulaMarkings(){
+
+	private void showMolecularFormulaMarkings() {
 		msviewer.showOverview();
 		this.zoomIn.setEnabled(false);
 		this.zoomOut.setEnabled(false);
@@ -293,8 +300,8 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 	@Override
 	public void peaksMarked(List<Integer> indices) {
 		this.model.removeMarkings();
-		for(int i : indices) this.model.setMarked(i, true);
-		if(indices.size()>0){
+		for (int i : indices) this.model.setMarked(i, true);
+		if (indices.size() > 0) {
 			this.zoomIn.setEnabled(true);
 			this.zoomInMI.setEnabled(true);
 		}
@@ -303,8 +310,8 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 	@Override
 	public void peaksMarkedPerDrag(List<Integer> indices) {
 		this.model.removeMarkings();
-		for(int i : indices) this.model.setMarked(i, true);
-		if(indices.size()>0){
+		for (int i : indices) this.model.setMarked(i, true);
+		if (indices.size() > 0) {
 			this.zoomIn.setEnabled(true);
 			this.zoomInMI.setEnabled(true);
 		}
@@ -313,32 +320,32 @@ public class SpectraVisualizationPanel extends JPanel implements ActionListener,
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(e.isPopupTrigger()){
-			this.zoomPopMenu.show(e.getComponent(), e.getX(), e.getY());			
+		if (e.isPopupTrigger()) {
+			this.zoomPopMenu.show(e.getComponent(), e.getX(), e.getY());
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(e.isPopupTrigger()){
-			this.zoomPopMenu.show(e.getComponent(), e.getX(), e.getY());			
+		if (e.isPopupTrigger()) {
+			this.zoomPopMenu.show(e.getComponent(), e.getX(), e.getY());
 		}
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
