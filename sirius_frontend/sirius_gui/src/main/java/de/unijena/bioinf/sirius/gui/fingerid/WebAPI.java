a3b1bd67d05293e2f7830ef25c1b39c7eef6ba39
kaidu <kai.duehrkop@uni-jena.de>
2017-02-03 17:20:40 +0100

    hotfix

--- sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/fingerid/WebAPI.java
+++ sirius_frontend/sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/fingerid/WebAPI.java
@@ -92,7 +92,7 @@ public class WebAPI implements Closeable {
 
 
     public static final String VERSION = "3.4"; //todo get from properties file
-    public static final String DATE = "2017-02-02";
+    public static final String DATE = "2017-02-03";
 
     public VersionsInfo needsUpdate() {
         final HttpGet get;
