3aeb0c98f26a4d5ba36e4e8125d7a1d0e6b35a3b
fleisch <markus.fleischauer@uni-jena.de>
2017-12-14 14:08:49 +0100

    better job manger auto init

--- chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/jobs/SiriusJobs.java
+++ chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/jobs/SiriusJobs.java
@@ -1,6 +1,8 @@
 package de.unijena.bioinf.ChemistryBase.jobs;
 
+import de.unijena.bioinf.ChemistryBase.properties.PropertyManager;
 import de.unijena.bioinf.jjobs.JobManager;
+import org.slf4j.LoggerFactory;
 
 public class SiriusJobs {
     private static JobManager globalJobManager = null;
@@ -16,7 +18,8 @@ public class SiriusJobs {
 
     public static JobManager getGlobalJobManager() {
         if (globalJobManager == null) {
-            setGlobalJobManager(1 + Runtime.getRuntime().availableProcessors() / 2);
+            setGlobalJobManager(PropertyManager.getNumberOfCores());
+            LoggerFactory.getLogger(SiriusJobs.class).info("Job manager initialized with " + globalJobManager.getCPUThreads() + "CPU threads and " + globalJobManager.getIOThreads() + " IO threads");
         }
         return globalJobManager;
     }
