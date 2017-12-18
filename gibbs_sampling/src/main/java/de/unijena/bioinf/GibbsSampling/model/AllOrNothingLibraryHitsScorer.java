eeee38599a8e2d4b346b05e978c38b1e90901d44
marcusL <m.ludwig@uni-jena.de>
2017-12-18 16:32:44 +0100

    bug fix change node scorer
    (cherry picked from commit d091c11)

--- gibbs_sampling/src/main/java/de/unijena/bioinf/GibbsSampling/model/AllOrNothingLibraryHitsScorer.java
+++ gibbs_sampling/src/main/java/de/unijena/bioinf/GibbsSampling/model/AllOrNothingLibraryHitsScorer.java
@@ -2,25 +2,21 @@ package de.unijena.bioinf.GibbsSampling.model;
 
 public class AllOrNothingLibraryHitsScorer implements NodeScorer<FragmentsCandidate>{
     @Override
-    public void score(FragmentsCandidate[][] candidates) {
-        for(int i = 0; i < candidates.length; ++i) {
-            FragmentsCandidate[] mfCandidates = candidates[i];
-
-            for(int j = 0; j < mfCandidates.length; ++j) {
-                FragmentsCandidate candidate = mfCandidates[j];
-                if(candidate.inEvaluationSet) {
-                    System.out.println("in Evaluation set: " + candidate.experiment.getName() + "\t" + candidate.getFormula());
-                    candidate.addNodeProbabilityScore(1d);//all get this score
-                } else if(candidate.hasLibraryHit()) {
-                    if (candidate.isCorrect()){
-                        candidate.addNodeProbabilityScore(1d);
-                        System.out.println("score correct library hit "+candidate.getExperiment().getName());
-                    }
-                    else candidate.addNodeProbabilityScore(0d);
-
-                } else {
-                    candidate.addNodeProbabilityScore(1d); //nothing changes
+    public void score(FragmentsCandidate[] candidates) {
+        for(int j = 0; j < candidates.length; ++j) {
+            FragmentsCandidate candidate = candidates[j];
+            if(candidate.inEvaluationSet) {
+                System.out.println("in Evaluation set: " + candidate.experiment.getName() + "\t" + candidate.getFormula());
+                candidate.addNodeProbabilityScore(1d);//all get this score
+            } else if(candidate.hasLibraryHit()) {
+                if (candidate.isCorrect()){
+                    candidate.addNodeProbabilityScore(1d);
+                    System.out.println("score correct library hit "+candidate.getExperiment().getName());
                 }
+                else candidate.addNodeProbabilityScore(0d);
+
+            } else {
+                candidate.addNodeProbabilityScore(1d); //nothing changes
             }
         }
     }
