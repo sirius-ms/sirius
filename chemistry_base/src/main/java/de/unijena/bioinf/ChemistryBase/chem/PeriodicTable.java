bd1cef017b317da64456488f816b34a3c3f0e001
fleisch <markus.fleischauer@uni-jena.de>
2018-01-17 15:52:11 +0100

    * compute panel logic for adducts

--- chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/chem/PeriodicTable.java
+++ chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/chem/PeriodicTable.java
@@ -216,7 +216,7 @@ public class PeriodicTable implements Iterable<Element>, Cloneable {
             likely.add(ionByName("[M+Na]+"));
             for (PrecursorIonType i : likely) ions.remove(i);
             likely.addAll(ions);
-        } else {
+        } else if (charge < 0) {
             likely.add(ionByName("[M-H]-"));
             likely.add(ionByName("[M]-"));
             for (PrecursorIonType i : likely) ions.remove(i);
@@ -903,6 +903,21 @@ public class PeriodicTable implements Iterable<Element>, Cloneable {
     }
 
 
+    public Set<PrecursorIonType> getAdducts() {
+        Set<PrecursorIonType> adducts = new HashSet<>(knownIonTypes.values().size() + 3);
+        adducts.addAll(knownIonTypes.values());
+        return adducts;
+    }
+
+    public Set<PrecursorIonType> getAdductsAndUnKnowns() {
+        Set<PrecursorIonType> adducts = getAdducts();
+        adducts.add(PrecursorIonType.unknown());
+        adducts.add(PrecursorIonType.unknownPositive());
+        adducts.add(PrecursorIonType.unknownNegative());
+        return adducts;
+    }
+
+
     public Set<PrecursorIonType> adductsByIonisation(Collection<PrecursorIonType> ionMode) {
         Set<PrecursorIonType> adducts = new HashSet<>();
         for (PrecursorIonType ionization : ionMode) {
@@ -939,6 +954,7 @@ public class PeriodicTable implements Iterable<Element>, Cloneable {
         Set<PrecursorIonType> r = ionizationToAdduct.get(p.getIonization().getName());
         return r == null ? Collections.<PrecursorIonType>emptySet() : Collections.unmodifiableSet(r);
     }
+
 
     /**
      * Calculate for a given alphabet the maximal and minimal mass defects of isotopes.
