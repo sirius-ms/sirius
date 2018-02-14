844c2810ff15f6d430638995d8cae5e2276be464
fleisch <markus.fleischauer@uni-jena.de>
2018-02-14 18:06:12 +0100

    naicer adduct sorting

--- sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/utils/PrecursorIonTypeSelector.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/utils/PrecursorIonTypeSelector.java
@@ -4,7 +4,7 @@ import de.unijena.bioinf.ChemistryBase.chem.PeriodicTable;
 import de.unijena.bioinf.ChemistryBase.chem.PrecursorIonType;
 
 import javax.swing.*;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
@@ -12,20 +12,35 @@ public class PrecursorIonTypeSelector extends JComboBox<String> {
     public static final String name = "Adduct";
 
     public PrecursorIonTypeSelector(Vector<String> adducts, String selected) {
-        Collections.sort(adducts);
         setModel(new DefaultComboBoxModel<>(adducts));
         setSelectedItem(selected);
     }
 
     public PrecursorIonTypeSelector(String selected) {
-        this(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().map(PrecursorIonType::toString).sorted().collect(Collectors.toList())), selected);
+        this(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().sorted(new PrecursorViewComparator().reversed()).map(PrecursorIonType::toString).collect(Collectors.toList())), selected);
     }
 
     public PrecursorIonTypeSelector() {
-        this(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().map(PrecursorIonType::toString).sorted().collect(Collectors.toList())), PeriodicTable.getInstance().unknownPositivePrecursorIonType().getIonization().getName());
+        this(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().sorted(new PrecursorViewComparator().reversed()).map(PrecursorIonType::toString).collect(Collectors.toList())), PeriodicTable.getInstance().unknownPositivePrecursorIonType().getIonization().getName());
     }
 
     public void refresh() {
-        setModel(new DefaultComboBoxModel<>(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().map(PrecursorIonType::toString).sorted().collect(Collectors.toList()))));
+        setModel(new DefaultComboBoxModel<>(new Vector<>(PeriodicTable.getInstance().getAdductsAndUnKnowns().stream().sorted(new PrecursorViewComparator().reversed()).map(PrecursorIonType::toString).collect(Collectors.toList()))));
+    }
+
+    static class PrecursorViewComparator implements Comparator<PrecursorIonType> {
+
+        @Override
+        public int compare(PrecursorIonType o1, PrecursorIonType o2) {
+            int r = Boolean.compare(o1.isIonizationUnknown(), o2.isIonizationUnknown());
+            if (r == 0) {
+                r = Boolean.compare(o1.getCharge() > 0, o2.getCharge() > 0);
+                if (r == 0) {
+                    r = o1.toString().compareTo(o2.toString());
+                }
+            }
+            return r;
+        }
     }
 }
+
