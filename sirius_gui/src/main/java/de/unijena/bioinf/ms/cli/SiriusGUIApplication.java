cf855a145ac6005857e185403cf99886e4ae20f6
kaidu <kai.duehrkop@uni-jena.de>
2018-02-20 12:33:22 +0100

    hotfix
    (cherry picked from commit 1d6879d)

--- sirius_gui/src/main/java/de/unijena/bioinf/ms/cli/SiriusGUIApplication.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/ms/cli/SiriusGUIApplication.java
@@ -5,8 +5,6 @@ package de.unijena.bioinf.ms.cli;
  * 15.06.16.
  */
 
-import com.lexicalscope.jewel.cli.CliFactory;
-import com.lexicalscope.jewel.cli.HelpRequestedException;
 import de.unijena.bioinf.ChemistryBase.jobs.SiriusJobs;
 import de.unijena.bioinf.ChemistryBase.properties.PropertyManager;
 import de.unijena.bioinf.jjobs.JobManager;
@@ -19,7 +17,6 @@ import de.unijena.bioinf.sirius.net.ProxyManager;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Arrays;
 
 /**
  * @author Markus Fleischauer (markus.fleischauer@gmail.com)
@@ -27,29 +24,19 @@ import java.util.Arrays;
 public class SiriusGUIApplication {
 
     public static void main(String[] args) {
+        /*
         final ZodiacCLI<SiriusGUIOptions> cli = new ZodiacCLI<>();
         cli.parseArgs(args, SiriusGUIOptions.class);
-
-        if (cli.options.isZodiac()) {
-            ZodiacOptions options = null;
-            try {
-                String[] newArgs = removeFromArrayIgnoreCase(args, "--zodiac");
-                options = CliFactory.createCli(ZodiacOptions.class).parseArguments(newArgs);
-            } catch (HelpRequestedException e) {
-                cli.println(e.getMessage());
-                cli.println("");
-                System.exit(0);
-            }
-
-            cli.setup();
-            cli.validate();
-            Zodiac zodiac = new Zodiac(options);
-            zodiac.run();
-        } else if (cli.options.isGUI()) {
+           */
+        boolean isGui = false;
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("--gui") || arg.equals("-u"))
+                isGui = true;
+        }
+        if (isGui) {
             final int cpuThreads = Integer.valueOf(PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.sirius.cpu.cores", "1"));
             SiriusJobs.setGlobalJobManager(new SwingJobManager(PropertyManager.getNumberOfThreads(), Math.min(cpuThreads, 3)));
             FingeridCLI.DEFAULT_LOGGER.info("Swing Job MANAGER initialized! " + SiriusJobs.getGlobalJobManager().getCPUThreads() + " : " + SiriusJobs.getGlobalJobManager().getIOThreads());
-
 
             if (ProxyManager.getProxyStrategy() == null) {
                 ApplicationCore.SIRIUS_PROPERTIES_FILE.setAndStoreProperty("de.unijena.bioinf.sirius.proxy", ProxyManager.DEFAULT_STRATEGY.name());
@@ -79,36 +66,8 @@ public class SiriusGUIApplication {
             FingeridCLI.DEFAULT_LOGGER.info("GUI initialized, showing GUI..");
             MainFrame.MF.decoradeMainFrameInstance();
         } else {
-            try {
-                cli.setup();
-                cli.validate();
-                cli.compute();
-            } finally {
-                try {
-                    JobManager.shutDownAllInstances();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    System.exit(0);
-                }
-            }
+            //this os save because the only difference between cli and gui parameters is the --gui param
+            SiriusCLIApplication.main(args);
         }
-
-    }
-
-    private static String[] removeFromArrayIgnoreCase(String[] args, String param) {
-        int idx = -1;
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equalsIgnoreCase(param)) {
-                idx = i;
-                break;
-            }
-
-        }
-        if (idx < 0) return args.clone();
-        String[] argsNew = Arrays.copyOf(args, args.length - 1);
-        for (int i = idx + 1; i < args.length; i++) {
-            argsNew[i - 1] = args[i];
-        }
-        return argsNew;
     }
 }
