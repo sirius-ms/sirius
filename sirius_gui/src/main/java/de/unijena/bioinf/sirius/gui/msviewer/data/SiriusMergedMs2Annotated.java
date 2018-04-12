5ad4a9ac383238d9f3e37f2a12bbee83a31da45b
marcusL <m.ludwig@uni-jena.de>
2018-04-12 15:08:07 +0200

    use precessedInput to display merged spectrum, if available
    (cherry picked from commit 55e8ec3)

--- sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/msviewer/data/SiriusMergedMs2Annotated.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/sirius/gui/msviewer/data/SiriusMergedMs2Annotated.java
@@ -5,19 +5,33 @@ import de.unijena.bioinf.ChemistryBase.ms.Ms2Experiment;
 import de.unijena.bioinf.ChemistryBase.ms.Peak;
 import de.unijena.bioinf.ChemistryBase.ms.Spectrum;
 import de.unijena.bioinf.ChemistryBase.ms.ft.FTree;
+import de.unijena.bioinf.ChemistryBase.ms.utils.SimpleMutableSpectrum;
 import de.unijena.bioinf.ChemistryBase.ms.utils.Spectrums;
+import de.unijena.bioinf.FragmentationTreeConstruction.model.ProcessedInput;
+import de.unijena.bioinf.FragmentationTreeConstruction.model.ProcessedPeak;
+
+import java.util.List;
 
 public class SiriusMergedMs2Annotated extends SiriusSingleSpectrumAnnotated {
 
     public SiriusMergedMs2Annotated(FTree tree, Ms2Experiment experiment, double minMz, double maxMz) {
-        super(tree, merge(experiment), minMz, maxMz);
+        super(tree, merge(experiment, tree), minMz, maxMz);
     }
 
     public SiriusMergedMs2Annotated(FTree tree, Ms2Experiment experiment) {
-        super(tree, merge(experiment));
+        super(tree, merge(experiment, tree));
     }
 
-    private static Spectrum<? extends Peak> merge(Ms2Experiment experiment) {
+    private static Spectrum<? extends Peak> merge(Ms2Experiment experiment, FTree tree) {
+        ProcessedInput processedInput = tree.getAnnotationOrNull(ProcessedInput.class);
+        if (processedInput!=null){
+            List<ProcessedPeak> peakList = processedInput.getMergedPeaks();
+            SimpleMutableSpectrum spectrum = new SimpleMutableSpectrum();
+            for (ProcessedPeak processedPeak : peakList) {
+                spectrum.addPeak(processedPeak);
+            }
+            return spectrum;
+        }
         return Spectrums.mergeSpectra(new Deviation(10, 0.1), true, false, experiment.getMs2Spectra());
     }
 }
