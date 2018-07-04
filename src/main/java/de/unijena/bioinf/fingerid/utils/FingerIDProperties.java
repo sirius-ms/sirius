a125ee283c6b2ffa0a8335ee3a4f581eac6bf6b4
fleisch <markus.fleischauer@uni-jena.de>
2018-07-04 09:36:43 +0200

    rename oss libs

---
+++ src/main/java/de/unijena/bioinf/fingerid/utils/FingerIDProperties.java
@@ -0,0 +1,42 @@
+package de.unijena.bioinf.fingerid.utils;
+
+
+import de.unijena.bioinf.ChemistryBase.properties.PropertyManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * All version numbers are directly taken from the build. So there is no more redundant data. Note that version a.b.c-SNAPSHOT lower than a.b.c
+ */
+public class FingerIDProperties {
+    static {
+        //init sirius credentials to global property manager
+        Path creds = Paths.get(System.getProperty("user.home"), "sirius.credentials");
+        PropertyManager.addPropertiesFromFile(creds);
+    }
+
+    public static String fingeridVersion() {
+        return PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.fingerid.version");
+    }
+
+    public static String fingeridWebHost() {
+        return PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.fingerid.web.host");
+    }
+
+    public static String fingeridWebPort() {
+        return PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.fingerid.web.port");
+    }
+
+    public static String databaseDate() {
+        return PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.fingerid.db.date");
+    }
+
+    public static String siriusVersion() {
+        return PropertyManager.PROPERTIES.getProperty("de.unijena.bioinf.sirius.version");
+    }
+
+    public static String sirius_guiVersion() {
+        return siriusVersion();
+    }
+}
