878346d4687dffa1e2d988fb06e1e9bf0933b10d
fleisch <markus.fleischauer@uni-jena.de>
2018-07-17 14:59:08 +0200

    biofilter removed from chemical db wrapper class to prefent old api for internal cli tools

---
+++ chemdb_sql_oss/src/main/java/de/unijena/bioinf/chemdb/FilteredChemicalDB.java
@@ -0,0 +1,98 @@
+package de.unijena.bioinf.chemdb;
+
+import de.unijena.bioinf.ChemistryBase.chem.InChI;
+import de.unijena.bioinf.ChemistryBase.chem.MolecularFormula;
+import de.unijena.bioinf.ChemistryBase.chem.PrecursorIonType;
+import de.unijena.bioinf.ChemistryBase.ms.Deviation;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+// this class is just a workaround to prevent old api for the internal csi Fingerid tools and should not
+// be used for new code
+@Deprecated
+public class FilteredChemicalDB extends AbstractChemicalDatabase implements Cloneable {
+
+
+    private BioFilter biofilter = BioFilter.ALL;
+    private final ChemicalDatabase wrappedDB;
+
+    public FilteredChemicalDB() throws DatabaseException {
+        super();
+        wrappedDB = new ChemicalDatabase();
+    }
+
+    public FilteredChemicalDB(ChemicalDatabase db) {
+        wrappedDB = db;
+    }
+
+    public BioFilter getBioFilter() {
+        return biofilter;
+    }
+
+    public void setBioFilter(BioFilter biofilter) {
+        this.biofilter = biofilter;
+    }
+
+    @Override
+    public List<FormulaCandidate> lookupMolecularFormulas(double mass, Deviation deviation, PrecursorIonType ionType) throws DatabaseException {
+        return wrappedDB.lookupMolecularFormulas(biofilter, mass, deviation, ionType);
+    }
+
+    @Override
+    public List<CompoundCandidate> lookupStructuresByFormula(MolecularFormula formula) throws DatabaseException {
+        return wrappedDB.lookupStructuresByFormula(biofilter, formula);
+    }
+
+
+    @Override
+    public <T extends Collection<FingerprintCandidate>> T lookupStructuresAndFingerprintsByFormula(MolecularFormula formula, T fingerprintCandidates) throws DatabaseException {
+        return wrappedDB.lookupStructuresAndFingerprintsByFormula(biofilter, formula, fingerprintCandidates);
+    }
+
+    @Override
+    public List<FingerprintCandidate> lookupFingerprintsByInchis(Iterable<String> inchi_keys) throws DatabaseException {
+        return wrappedDB.lookupFingerprintsByInchis(inchi_keys);
+    }
+
+    @Override
+    public List<InChI> lookupManyInchisByInchiKeys(Iterable<String> inchi_keys) throws DatabaseException {
+        return wrappedDB.lookupManyInchisByInchiKeys(inchi_keys);
+    }
+
+    @Override
+    public List<FingerprintCandidate> lookupManyFingerprintsByInchis(Iterable<String> inchi_keys) throws DatabaseException {
+        return wrappedDB.lookupManyFingerprintsByInchis(inchi_keys);
+    }
+
+    @Override
+    public List<FingerprintCandidate> lookupFingerprintsByInchi(Iterable<CompoundCandidate> compounds) throws DatabaseException {
+        return wrappedDB.lookupFingerprintsByInchi(compounds);
+    }
+
+    @Override
+    public List<InChI> findInchiByNames(List<String> names) throws DatabaseException {
+        return wrappedDB.findInchiByNames(names);
+    }
+
+    @Override
+    public void annotateCompounds(List<? extends CompoundCandidate> sublist) throws DatabaseException {
+        wrappedDB.annotateCompounds(sublist);
+    }
+
+    @Override
+    public void close() throws IOException {
+        wrappedDB.close();
+    }
+
+    public FilteredChemicalDB clone(){
+        FilteredChemicalDB clone = new FilteredChemicalDB(wrappedDB.clone()); //todo maybe we should not clone wrapped db here
+        clone.setBioFilter(biofilter);
+        return clone;
+    }
+
+    public boolean isWrappedDBChemDB(){
+        return wrappedDB instanceof ChemicalDatabase;
+    }
+}
