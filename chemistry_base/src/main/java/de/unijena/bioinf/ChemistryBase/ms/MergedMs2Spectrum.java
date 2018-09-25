11421e7d2a3c970473af9c9b6816e33ef193a491
marcusL <m.ludwig@uni-jena.de>
2018-09-25 10:57:27 +0200

    constructors in MergedMs2Spectrum

--- chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/ms/MergedMs2Spectrum.java
+++ chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/ms/MergedMs2Spectrum.java
@@ -1,4 +1,15 @@
 package de.unijena.bioinf.ChemistryBase.ms;
 
 public class MergedMs2Spectrum extends MutableMs2Spectrum implements Ms2ExperimentAnnotation {
+
+    public MergedMs2Spectrum() {
+    }
+
+    public <T extends Peak, S extends Spectrum<T>> MergedMs2Spectrum(S spec, double parentmass, CollisionEnergy energy, int mslevel) {
+        super(spec, parentmass, energy, mslevel);
+    }
+
+    public MergedMs2Spectrum(Spectrum<? extends Peak> spec) {
+        super(spec);
+    }
 }
