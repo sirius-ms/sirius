8a4abf008e8ed3e9daedc464100b22c86aa7c876
fleisch <markus.fleischauer@uni-jena.de>
2018-10-12 11:02:09 +0200

    biotransformation api

--- chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/chem/utils/biotransformation/BioTransformer.java
+++ chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/chem/utils/biotransformation/BioTransformer.java
@@ -3,7 +3,11 @@ package de.unijena.bioinf.ChemistryBase.chem.utils.biotransformation;
 import de.unijena.bioinf.ChemistryBase.chem.MolecularFormula;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by fleisch on 29.05.17.
@@ -18,11 +22,23 @@ public class BioTransformer {
         return ts;
     }
 
+    public static List<MolecularFormula> transform(final MolecularFormula source, Collection<BioTransformation> transformations) {
+        return transform(source, transformations.stream());
+    }
+
+    public static List<MolecularFormula> transform(final MolecularFormula source, BioTransformation[] transformations) {
+        return transform(source, Arrays.stream(transformations));
+    }
+
+    public static List<MolecularFormula> transform(final MolecularFormula source, Stream<BioTransformation> transformations) {
+        return transformations.distinct().map((trans) -> transform(source, trans)).flatMap((List::stream)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
     public static List<MolecularFormula> transform(MolecularFormula source, BioTransformation transformation) {
         List<MolecularFormula> ts = new ArrayList<>(2);
         if (transformation.isConditional()) {
             if (source.contains(transformation.getCondition())) {
-                ts.add(transform(source, transformation.getCondition(),transformation.getFormula()));
+                ts.add(transform(source, transformation.getCondition(), transformation.getFormula()));
             }
             if (transformation.isSymmetric() && source.contains(transformation.getFormula())) {
                 ts.add(transform(source, transformation.getFormula(), transformation.getCondition()));
