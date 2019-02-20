1e1b642082db1443ca5338a8cf7e29fa571fd099
fleisch <markus.fleischauer@uni-jena.de>
2019-02-20 13:44:55 +0100

    minor stuff and docker experiment

--- confidence_score_predict_oss/src/test/java/de/unijena/bioinf/confidence_score/Main.java
+++ confidence_score_predict_oss/src/test/java/de/unijena/bioinf/confidence_score/Main.java
@@ -1,39 +1,12 @@
 package de.unijena.bioinf.confidence_score;
 
-import de.unijena.bioinf.ChemistryBase.algorithm.Scored;
-import de.unijena.bioinf.ChemistryBase.chem.CompoundWithAbstractFP;
-import de.unijena.bioinf.ChemistryBase.fp.ProbabilityFingerprint;
-import de.unijena.bioinf.ChemistryBase.ms.Ms2Experiment;
-import de.unijena.bioinf.chemdb.BioFilter;
-import de.unijena.bioinf.chemdb.ChemicalDatabase;
-import de.unijena.bioinf.chemdb.CompoundCandidate;
-import de.unijena.bioinf.chemdb.FingerprintCandidate;
-import de.unijena.bioinf.confidence_score.features.DistanceFeatures;
-import de.unijena.bioinf.confidence_score.features.LogDistanceFeatures;
-import de.unijena.bioinf.confidence_score.features.PvalueScoreUtils;
-import de.unijena.bioinf.confidence_score.features.ScoreFeatures;
-import de.unijena.bioinf.confidence_score.svm.SVMPredict;
-import de.unijena.bioinf.confidence_score.svm.SVMUtils;
-import de.unijena.bioinf.confidence_score.svm.TrainedSVM;
-import de.unijena.bioinf.fingerid.Prediction;
-import de.unijena.bioinf.fingerid.SpectralPreprocessor;
-import de.unijena.bioinf.fingerid.blast.Fingerblast;
-import de.unijena.bioinf.fingerid.blast.ScoringMethodFactory;
-import de.unijena.bioinf.sirius.IdentificationResult;
-import de.unijena.bioinf.sirius.Sirius;
-
-import java.io.File;
-import java.lang.reflect.Executable;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by martin on 20.06.18.
  */
 public class Main {
 
 
-    public static void main(String[] args){
+    /*public static void main(String[] args){
 
        //TODO: Read a compound and see if everything works
 
@@ -80,7 +53,7 @@ public class Main {
 
                 Scored<FingerprintCandidate>[] condesnsed = utils.condense_candidates_by_flag(scored_array, 2);
 
-         /*       CombinedFeatureCreator comb = new CombinedFeatureCreator(new ScoreFeatures(ScoringMethodFactory.getCSIFingerIdScoringMethod(prediction.getFingerid().getPredictionPerformances()).getScoring()),
+         *//*       CombinedFeatureCreator comb = new CombinedFeatureCreator(new ScoreFeatures(ScoringMethodFactory.getCSIFingerIdScoringMethod(prediction.getFingerid().getPredictionPerformances()).getScoring()),
                         new DistanceFeatures(1, 2), new LogDistanceFeatures(1, 2));
 
                 comb.prepare(prediction.getFingerid().getPredictionPerformances());
@@ -90,7 +63,7 @@ public class Main {
                 featureMatrix[0]=feature;
                 SVMPredict predict = new SVMPredict();
 
-                predict.predict_confidence(featureMatrix,new TrainedSVM(trained_svm));*/
+                predict.predict_confidence(featureMatrix,new TrainedSVM(trained_svm));*//*
 
             }
 
@@ -110,5 +83,5 @@ public class Main {
             e.printStackTrace();;
         }
 
-    }
+    }*/
 }
