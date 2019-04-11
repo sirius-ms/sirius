3e8e5b2c90dc04a00ad33787c02cdc08622fb58a
fleisch <markus.fleischauer@uni-jena.de>
2019-04-11 15:29:17 +0200

    csi fingerid for new cli implemented. API improved. confidence score integration prepared NON_DEBUGGED code.

--- src/test/java/de/unijena/bioinf/ms/projectspace/ProjectSpaceTest.java
+++ src/test/java/de/unijena/bioinf/ms/projectspace/ProjectSpaceTest.java
@@ -15,12 +15,11 @@ import java.util.Properties;
 public class ProjectSpaceTest {
 
     public static void main(String[] args) throws IOException {
-        Properties props = PropertyManager.PROPERTIES;
-        props.setProperty("de.unijena.bioinf.fingerid.web.host","https://www.csi-fingerid.uni-jena.de");
-        props.setProperty("de.unijena.bioinf.fingerid.db.date","2017-08-28");
-        props.setProperty("de.unijena.bioinf.sirius.version","4.0.4-SNAPSHOT");
-        props.setProperty("de.unijena.bioinf.fingerid.version","1.1.4-SNAPSHOT");
-        props.setProperty("de.unijena.bioinf.sirius.fingerID.cache", "/home/fleisch/.sirius/csi_fingerid_cache");
+        PropertyManager.setProperty("de.unijena.bioinf.fingerid.web.host","https://www.csi-fingerid.uni-jena.de");
+        PropertyManager.setProperty("de.unijena.bioinf.fingerid.db.date","2017-08-28");
+        PropertyManager.setProperty("de.unijena.bioinf.sirius.version","4.0.5-SNAPSHOT");
+        PropertyManager.setProperty("de.unijena.bioinf.fingerid.version","1.1.5-SNAPSHOT");
+        PropertyManager.setProperty("de.unijena.bioinf.sirius.fingerID.cache", "/home/fleisch/.sirius/csi_fingerid_cache");
 //        File root = new File("/home/fleisch/work/sirius_testing/ws (copy)");
         File root = new File("/home/fleisch/work/sirius_testing/CSIfingerID_output_BA_QE_2iso5 (copy)");
         if (args.length > 0)
