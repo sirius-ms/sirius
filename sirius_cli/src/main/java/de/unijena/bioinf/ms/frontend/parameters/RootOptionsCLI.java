15789f95f6e9df968833291c3a35f5e447c8496b
fleisch <markus.fleischauer@uni-jena.de>
2019-06-05 03:22:10 +0200

    Zodiac serializer

--- sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/parameters/RootOptionsCLI.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/parameters/RootOptionsCLI.java
@@ -248,6 +248,7 @@ public class RootOptionsCLI implements RootOptions {
         // the calculators are not available -> e.g. web connection!
         return new MetaDataSerializer[]{
                 new IdentificationResultSerializer()
+                , new ZoadiacResultSerializer()
                 , new FingerIdResultSerializer(ApplicationCore.WEB_API)
 //                , new CanopusResultSerializer(ApplicationCore.CANOPUS)
         };
