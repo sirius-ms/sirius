aed7cf5b9b87a73f4d4b361dd20e9e4cc52af03c
Kai Dührkop <kai.duehrkop@uni-jena.de>
2019-09-06 13:38:43 +0300

    static classes to setup the projectspace configuration

---
+++ src/main/java/de/unijena/bioinf/projectspace/FingerIdProjectSpaceConfiguration.java
@@ -0,0 +1,25 @@
+package de.unijena.bioinf.projectspace;
+
+import de.unijena.bioinf.ChemistryBase.fp.ProbabilityFingerprint;
+import de.unijena.bioinf.fingerid.CanopusResult;
+import de.unijena.bioinf.fingerid.FingerprintResult;
+import de.unijena.bioinf.fingerid.blast.FingerblastResult;
+import de.unijena.bioinf.projectspace.fingerid.*;
+import de.unijena.bioinf.projectspace.sirius.FormulaResult;
+
+/**
+ * has to be called on startup to setup the project space
+ */
+public class FingerIdProjectSpaceConfiguration {
+
+    public static void configure(ProjectSpaceConfiguration configuration) {
+
+        configuration.registerComponent(FormulaResult.class, FingerprintResult.class, new FingerprintSerializer());
+        configuration.registerComponent(FormulaResult.class, CanopusResult.class , new CanopusSerializer());
+        configuration.registerComponent(FormulaResult.class, FingerblastResult.class, new FingerblastResultSerializer());
+
+        configuration.defineProjectSpaceProperty(CSIClientData.class, new CsiClientSerializer());
+        configuration.defineProjectSpaceProperty(CanopusClientData.class, new CanopusClientSerializer());
+    }
+
+}
--- src/main/java/de/unijena/bioinf/projectspace/fingerid/FingerprintSerializer.java
+++ src/main/java/de/unijena/bioinf/projectspace/fingerid/FingerprintSerializer.java
@@ -3,26 +3,27 @@ package de.unijena.bioinf.projectspace.fingerid;
 import de.unijena.bioinf.ChemistryBase.fp.Fingerprint;
 import de.unijena.bioinf.ChemistryBase.fp.ProbabilityFingerprint;
 import de.unijena.bioinf.fingerid.CanopusResult;
+import de.unijena.bioinf.fingerid.FingerprintResult;
 import de.unijena.bioinf.projectspace.*;
 import de.unijena.bioinf.projectspace.sirius.FormulaResult;
 
 import java.io.IOException;
 
-public class FingerprintSerializer implements ComponentSerializer<FormulaResultId, FormulaResult, ProbabilityFingerprint> {
+public class FingerprintSerializer implements ComponentSerializer<FormulaResultId, FormulaResult, FingerprintResult> {
 
     @Override
-    public ProbabilityFingerprint read(ProjectReader reader, FormulaResultId id, FormulaResult container) throws IOException {
+    public FingerprintResult read(ProjectReader reader, FormulaResultId id, FormulaResult container) throws IOException {
         String loc = FingerIdLocations.FingerprintDir + "/" + id.fileName("fpt");
         if (!reader.exists(loc)) return null;
         final CSIClientData csiClientData = reader.getProjectSpaceProperty(CSIClientData.class);
         final double[] probabilities = reader.doubleVector(loc);
-        return new ProbabilityFingerprint(csiClientData.getFingerprintVersion(), probabilities);
+        return new FingerprintResult(new ProbabilityFingerprint(csiClientData.getFingerprintVersion(), probabilities));
     }
 
     @Override
-    public void write(ProjectWriter writer, FormulaResultId id, FormulaResult container, ProbabilityFingerprint component) throws IOException {
+    public void write(ProjectWriter writer, FormulaResultId id, FormulaResult container, FingerprintResult component) throws IOException {
         writer.inDirectory(FingerIdLocations.FingerprintDir, ()->{
-            writer.doubleVector(id.fileName("fpt"), component.toProbabilityArray());
+            writer.doubleVector(id.fileName("fpt"), component.fingerprint.toProbabilityArray());
             return true;
         });
     }
