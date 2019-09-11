dfe823f60205062e7b2dcc21ddd392c0c12a5304
Kai Dührkop <kai.duehrkop@uni-jena.de>
2019-09-11 14:45:31 +0300

    store ms2experiment when creating a compound container

--- src/main/java/de/unijena/bioinf/fingerid/blast/TopFingerblastScore.java
+++ src/main/java/de/unijena/bioinf/fingerid/blast/TopFingerblastScore.java
@@ -11,6 +11,6 @@ public class TopFingerblastScore extends FormulaScore {
 
     @Override
     public ScoreType getScoreType() {
-        return ScoreType.Probabilistic; //todo @Kai correct?
+        return ScoreType.Logarithmic; //todo @Kai correct?
     }
 }
