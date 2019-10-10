6727f67e670f4b93c2664138c7aa77af2f2d5bb7
fleisch <markus.fleischauer@uni-jena.de>
2019-10-10 12:13:37 +0200

    revert change

--- chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/math/HighQualityRandom.java
+++ chemistry_base/src/main/java/de/unijena/bioinf/ChemistryBase/math/HighQualityRandom.java
@@ -43,7 +43,7 @@ public class HighQualityRandom extends Random {
             v ^= v >>> 17;
             v ^= v << 31;
             v ^= v >>> 8;
-            w = 4294957665L * (w) + (w >>> 32);
+            w = 4294957665L * (w & 0xffffffff) + (w >>> 32);
             long x = u ^ (u << 21);
             x ^= x >>> 35;
             x ^= x << 4;
