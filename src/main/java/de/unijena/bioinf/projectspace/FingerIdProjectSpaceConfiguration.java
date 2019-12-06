610f048478cb6ec5a73101c45764705eee14bb08
fleisch <markus.fleischauer@uni-jena.de>
2019-12-06 22:25:39 +0100

    tmeporary alow of messed up refactoring

--- src/main/java/de/unijena/bioinf/projectspace/FingerIdProjectSpaceConfiguration.java
+++ src/main/java/de/unijena/bioinf/projectspace/FingerIdProjectSpaceConfiguration.java
@@ -1,7 +1,5 @@
 package de.unijena.bioinf.projectspace;
 
-import de.unijena.bioinf.ChemistryBase.fp.ProbabilityFingerprint;
-import de.unijena.bioinf.chemdb.CompoundCandidate;
 import de.unijena.bioinf.fingerid.CanopusResult;
 import de.unijena.bioinf.fingerid.FingerprintResult;
 import de.unijena.bioinf.fingerid.blast.FingerblastResult;
