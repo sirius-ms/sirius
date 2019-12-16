fdc1ed54ffa7504f4c14891732702b66fe4e2c4a
fleisch <markus.fleischauer@uni-jena.de>
2019-12-16 15:22:44 +0100

    readical cleanup, prepared for new rest-db

--- src/main/java/de/unijena/bioinf/ms/rest/model/worker/WorkerState.java
+++ src/main/java/de/unijena/bioinf/ms/rest/model/worker/WorkerState.java
@@ -1,5 +1,6 @@
 package de.unijena.bioinf.ms.rest.model.worker;
 
 public enum WorkerState {
+    //this are the states a worker can have -> stopping mean it will shut down itself
     STARTING, RUNNING, STOPPING;
 }
