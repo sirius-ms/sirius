c28431e45056b072622cfde05f05cb2433cc2f8d
fleisch <markus.fleischauer@uni-jena.de>
2019-12-19 22:27:35 +0100

    canopus worker implemented but untested

--- src/main/java/de/unijena/bioinf/ms/rest/model/canopus/CanopusJobInput.java
+++ src/main/java/de/unijena/bioinf/ms/rest/model/canopus/CanopusJobInput.java
@@ -2,14 +2,12 @@ package de.unijena.bioinf.ms.rest.model.canopus;
 
 import de.unijena.bioinf.fingerid.predictor_types.PredictorType;
 
-import java.util.EnumSet;
-
 /**
  * Class containing the input for Canopus Jobs
  * Will be (De-)Marshaled to/from json
  */
 public class CanopusJobInput {
-    protected final String formula;
+    public final String formula;
     public final byte[] fingerprint; // LITTLE ENDIAN BINARY ENCODED PLATT PROBABILITIES
     public final PredictorType predictor;
 
