0fb8e176b3a10fe429a555d93a88ffc40c8a2816
fleisch <markus.fleischauer@uni-jena.de>
2020-01-14 19:13:56 +0100

    a bit refactoring, implemented compatible single compound mode import, coin-or

---
+++ io/src/main/java/de/unijena/bioinf/babelms/CsvMSParser.java
@@ -0,0 +1,13 @@
+package de.unijena.bioinf.babelms;
+
+import de.unijena.bioinf.ChemistryBase.ms.Ms2Experiment;
+import de.unijena.bioinf.babelms.ms.CsvParser;
+
+import java.io.File;
+import java.util.List;
+
+public class CsvMSParser {
+
+
+
+}
--- io/src/main/java/de/unijena/bioinf/babelms/SpectralParser.java
+++ io/src/main/java/de/unijena/bioinf/babelms/SpectralParser.java
@@ -17,41 +17,30 @@
  */
 package de.unijena.bioinf.babelms;
 
-import de.unijena.bioinf.ChemistryBase.ms.Ms2Spectrum;
 import de.unijena.bioinf.ChemistryBase.ms.Peak;
+import de.unijena.bioinf.ChemistryBase.ms.Spectrum;
 import de.unijena.bioinf.ChemistryBase.utils.FileUtils;
-import de.unijena.bioinf.babelms.mgf.MgfParser;
-import de.unijena.bioinf.babelms.ms.CsvParser;
 
 import java.io.*;
 import java.util.Iterator;
 
 public abstract class SpectralParser {
 
-    public static SpectralParser getParserFor(File f) {
-        final String name = f.getName();
-        if (name.endsWith(".mgf")) {
-            return new MgfParser();
-        } else {
-            return new CsvParser();
-        }
-    }
-
-    public Iterator<Ms2Spectrum<Peak>> parseSpectra(File file) throws IOException {
+    public Iterator<? extends Spectrum<Peak>> parseSpectra(File file) throws IOException {
         final BufferedReader reader = FileUtils.ensureBuffering(new FileReader(file));
         return parseSpectra(reader);
     }
 
-    public Iterator<Ms2Spectrum<Peak>> parseSpectra(InputStream instream) throws IOException {
+    public Iterator<? extends Spectrum<Peak>> parseSpectra(InputStream instream) throws IOException {
         final BufferedReader reader = FileUtils.ensureBuffering(new InputStreamReader(instream));
         return parseSpectra(reader);
     }
 
-    public Iterator<Ms2Spectrum<Peak>> parseSpectra(Reader reader) throws IOException {
+    public Iterator<? extends Spectrum<Peak>> parseSpectra(Reader reader) throws IOException {
         if (!(reader instanceof BufferedReader)) reader = FileUtils.ensureBuffering(reader);
         return parseSpectra((BufferedReader)reader);
     }
 
-    public abstract Iterator<Ms2Spectrum<Peak>> parseSpectra(BufferedReader reader) throws IOException;
+    public abstract Iterator<? extends Spectrum<Peak>> parseSpectra(BufferedReader reader) throws IOException;
 
 }
