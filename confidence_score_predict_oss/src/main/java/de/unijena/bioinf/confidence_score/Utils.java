bbe5bbeef6fb0dd4d75d72dd331e6210ffe42bd1
fleisch <markus.fleischauer@uni-jena.de>
2020-03-20 15:39:03 +0100

    added filter helper method, fixed filter without 0 handling bugs

--- confidence_score_predict_oss/src/main/java/de/unijena/bioinf/confidence_score/Utils.java
+++ confidence_score_predict_oss/src/main/java/de/unijena/bioinf/confidence_score/Utils.java
@@ -1,6 +1,7 @@
 package de.unijena.bioinf.confidence_score;
 
 import de.unijena.bioinf.ChemistryBase.algorithm.scoring.Scored;
+import de.unijena.bioinf.chemdb.ChemDBs;
 import de.unijena.bioinf.chemdb.FingerprintCandidate;
 
 import java.util.Arrays;
@@ -10,34 +11,7 @@ public class Utils {
     private Utils() {
     }
 
-    public static Predicate<FingerprintCandidate> getCandidateByFlagFilter(final long dbFlag) {
-        return it -> (dbFlag == 0 || (it.getBitset() & dbFlag) != 0);
-    }
-
-    public static Scored<FingerprintCandidate>[] condenseCandidatesByFlag(Scored<FingerprintCandidate>[] candidates, long flags) {
-        /*if (flags == 0)
-            return candidates;
-
-
-        Scored<FingerprintCandidate>[] condensed;
-        ArrayList<Scored<FingerprintCandidate>> condensed_as_list= new ArrayList<>();
-
-
-        for(int i=0;i<candidates.length;i++){
-            if ((candidates[i].getCandidate().getBitset() & flags)!=0){
-                condensed_as_list.add(candidates[i]);
-            }
-
-        }
-
-        condensed = new Scored[condensed_as_list.size()];
-
-        condensed=  condensed_as_list.toArray(condensed);*/
-
-
-//        return condensed;
-
-        //TODO @Markus cleanup reverted weil error
-        return Arrays.stream(candidates).filter(s -> getCandidateByFlagFilter(flags).test(s.getCandidate())).toArray(Scored[]::new);
+    public static Scored<FingerprintCandidate>[] condenseCandidatesByFlag(Scored<FingerprintCandidate>[] candidates, final long flags) {
+        return Arrays.stream(candidates).filter(ChemDBs.inFilter(s -> s.getCandidate().getBitset(),flags)).toArray(Scored[]::new);
     }
 }
