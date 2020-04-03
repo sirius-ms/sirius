b730e9462c5ee9d2675233640879dec28541a46d
fleisch <markus.fleischauer@uni-jena.de>
2020-04-03 10:51:17 +0200

    minor

---
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/subtools/spectra_search/SpectraSearchWorkflow.java
@@ -0,0 +1,10 @@
+package de.unijena.bioinf.ms.frontend.subtools.spectra_search;
+
+import de.unijena.bioinf.ms.frontend.workflow.Workflow;
+
+public class SpectraSearchWorkflow implements Workflow {
+    @Override
+    public void run() {
+
+    }
+}
