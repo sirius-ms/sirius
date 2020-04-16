0af82bda4765f9ee4bc308b18263a70226023c84
Nils Alexander Haupt <nils.alexander.haupt@uni-jena.de>
2020-04-16 22:52:53 +0200

    FormulaResultController: getAnnotatedFormulaResult

--- sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/formulas/FormulaResultController.java
+++ sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/formulas/FormulaResultController.java
@@ -4,7 +4,6 @@ import de.unijena.bioinf.ChemistryBase.ms.ft.FTree;
 import de.unijena.bioinf.ms.annotations.DataAnnotation;
 import de.unijena.bioinf.ms.middleware.BaseApiController;
 import de.unijena.bioinf.ms.middleware.SiriusContext;
-import de.unijena.bioinf.projectspace.FormulaResultId;
 import de.unijena.bioinf.projectspace.FormulaScoring;
 import de.unijena.bioinf.projectspace.SiriusProjectSpace;
 import de.unijena.bioinf.projectspace.sirius.CompoundContainer;
@@ -56,14 +55,7 @@ public class FormulaResultController extends BaseApiController {
     @GetMapping(value = "/formulas/{fid}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public FormulaId getFormulaResult(@PathVariable String pid, @PathVariable String cid, @PathVariable String fid) {
         SiriusProjectSpace space = projectSpace(pid);
-        return getCompound(space, cid).map(c ->
-                c.findResult(fid).map(ffid -> {
-                    try {
-                        return space.getFormulaResult(ffid, FormulaScoring.class);
-                    } catch (IOException e) {
-                        return null;
-                    }
-                }).orElse(null)).
+        return this.getAnnotatedFormulaResult(space,cid,fid,FormulaScoring.class).
                 map(fr -> {
                     FormulaId formulaId = new FormulaId(fr.getId());
                     fr.getAnnotation(FormulaScoring.class).ifPresent(fs -> formulaId.setResultScores(new FormulaResultScores(fs)));
@@ -71,22 +63,25 @@ public class FormulaResultController extends BaseApiController {
         }).orElse(null);
     }
 
-    @RequestMapping(value = "formulas/{fid}/tree", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "formulas/{fid}/tree", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public FTree getFragmentationTree(@PathVariable String pid, @PathVariable String cid, @PathVariable String fid){
         SiriusProjectSpace projectSpace = super.projectSpace(pid);
-        Optional<FormulaResult> fResult = this.getCompound(projectSpace,cid).map(cc -> {
-            Optional<FormulaResultId> fResultId = cc.findResult(fid);
-            return fResultId.map(frId -> {
-                try {
-                    return projectSpace.getFormulaResult(frId, FTree.class);
-                } catch (IOException e) {
-                    return null;
-                }
-            }).orElse(null);
-        });
+        Optional<FormulaResult> fResult = this.getAnnotatedFormulaResult(projectSpace, cid, fid, FTree.class);
         FTree fTree = fResult.map(fr -> fr.getAnnotation(FTree.class).orElse(null)).orElse(null);
-        return fTree;
+        return fTree; //TODO: additional class for fragmentation tree whose objects can be displayed
         // The FTree object needs an extra class for JSON. It can't be displayed well. Nevertheless, it works with attributes of the FTree object.
+    }
+
+    private Optional<FormulaResult> getAnnotatedFormulaResult(SiriusProjectSpace projectSpace, String cid, String fid, Class<? extends DataAnnotation>... components){
+        Optional<FormulaResult> fResult = this.getCompound(projectSpace, cid).map(cc -> cc.findResult(fid).map(frId -> {
+            try {
+                return projectSpace.getFormulaResult(frId, components);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).orElse(null));
+        return fResult;
     }
 
     private Optional<CompoundContainer> getCompound(SiriusProjectSpace space, String cid, Class<? extends DataAnnotation>... components) {
