1569e20f65c6399572e1d66146bdc67aebb815b2
fleisch <markus.fleischauer@uni-jena.de>
2020-05-15 12:33:13 +0200

    changed SummaryLocations to .tsv file extension for better excel compatibility.

--- sirius_cli/src/main/java/de/unijena/bioinf/projectspace/summaries/SummaryLocations.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/projectspace/summaries/SummaryLocations.java
@@ -3,10 +3,10 @@ package de.unijena.bioinf.projectspace.summaries;
 public interface SummaryLocations {
     String
             MZTAB_SUMMARY = "report.mztab",
-            FORMULA_CANDIDATES = "formula_candidates.csv",
-            STRUCTURE_CANDIDATES = "structure_candidates.csv",
-            STRUCTURE_CANDIDATES_TOP = "structure_candidates_topHits.csv",
-            FORMULA_SUMMARY = "formula_identifications.csv",
-            COMPOUND_SUMMARY = "compound_identifications.csv",
-            COMPOUND_SUMMARY_ADDUCTS = "compound_identifications_adducts.csv";
+            FORMULA_CANDIDATES = "formula_candidates.tsv",
+            STRUCTURE_CANDIDATES = "structure_candidates.tsv",
+            STRUCTURE_CANDIDATES_TOP = "structure_candidates_topHits.tsv",
+            FORMULA_SUMMARY = "formula_identifications.tsv",
+            COMPOUND_SUMMARY = "compound_identifications.tsv",
+            COMPOUND_SUMMARY_ADDUCTS = "compound_identifications_adducts.tsv";
 }
