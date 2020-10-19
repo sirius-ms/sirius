1aed7609a45545bc265406209449cd18cfc8793f
fleisch <markus.fleischauer@uni-jena.de>
2020-10-19 19:30:06 +0200

    working windows msi packager with jdk11 and given app-image

--- sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/core/ApplicationCore.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/core/ApplicationCore.java
@@ -83,7 +83,7 @@ public abstract class ApplicationCore {
             t1 = System.currentTimeMillis();
         measureTime("Start AppCore");
         try {
-//            System.out.println(System.getProperty("java.library.path"));
+//            System.out.println("LD ==> " + System.getProperty("java.library.path"));
             System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
             org.apache.log4j.Logger.getLogger("net.sf.jnati").setLevel(Level.WARN);
 
