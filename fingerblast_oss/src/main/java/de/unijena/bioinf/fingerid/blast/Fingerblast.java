e965c42c146549a9263f13ee4a096a0dcec1d2d1
fleisch <markus.fleischauer@uni-jena.de>
2020-11-03 15:01:31 +0100

    removed fingerblast hack and added super parameter object that contains all parameters for all scorings

--- fingerblast_oss/src/main/java/de/unijena/bioinf/fingerid/blast/Fingerblast.java
+++ fingerblast_oss/src/main/java/de/unijena/bioinf/fingerid/blast/Fingerblast.java
@@ -68,20 +68,10 @@ public class Fingerblast<P> {
         this.scoringMethod = scoringMethod;
     }
 
-    @Deprecated // this ist just a legacy method for the fingerid_cli that should be replaced
-    public List<Scored<FingerprintCandidate>> search(@NotNull MolecularFormula formula, @NotNull ProbabilityFingerprint fingerprint) throws ChemicalDatabaseException {
-        return search(formula,fingerprint, (P) new FpParameters(fingerprint));
-    }
     public List<Scored<FingerprintCandidate>> search(@NotNull MolecularFormula formula, @NotNull ProbabilityFingerprint fingerprint, @Nullable P parameter) throws ChemicalDatabaseException {
         final List<FingerprintCandidate> candidates = searchEngine.lookupStructuresAndFingerprintsByFormula(formula);
         return score(candidates, fingerprint, parameter);
     }
-
-    @Deprecated // this ist just a legacy method for the fingerid_cli that should be replaced
-    public List<Scored<FingerprintCandidate>> score(@NotNull List<FingerprintCandidate> candidates, @NotNull ProbabilityFingerprint fingerprint) throws ChemicalDatabaseException {
-        return score(candidates,fingerprint, (P) new FpParameters(fingerprint));
-    }
-
 
     public List<Scored<FingerprintCandidate>> score(@NotNull List<FingerprintCandidate> candidates, @NotNull ProbabilityFingerprint fingerprint, @Nullable P parameter) throws ChemicalDatabaseException {
         //this would make the P parameter obsolete
