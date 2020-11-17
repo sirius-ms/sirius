1426800c213bd7727df42e50925dea2bc76adcff
fleisch <markus.fleischauer@uni-jena.de>
2020-11-17 12:32:56 +0100

    removed unused import

--- sirius_gui/src/main/java/de/unijena/bioinf/projectspace/GuiProjectSpaceManager.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/projectspace/GuiProjectSpaceManager.java
@@ -32,7 +32,6 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
