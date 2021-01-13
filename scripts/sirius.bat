af619841ee90338b7d11807c4d8c1b0f0a4de3d6
fleisch <markus.fleischauer@uni-jena.de>
2021-01-13 13:52:42 +0100

    fixed windows batch script

--- scripts/sirius.bat
+++ scripts/sirius.bat
@@ -27,7 +27,8 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%..
+@rem remove one \ from path
+set APP_HOME=%DIRNAME:~0,-1%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
