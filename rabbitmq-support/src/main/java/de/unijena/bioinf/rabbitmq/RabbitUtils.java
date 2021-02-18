3f5287542428b3c8dc37634b81ee3bdc0cf564e2
fleisch <markus.fleischauer@uni-jena.de>
2021-02-18 17:54:18 +0100

    rename, basic exchange infrastructure, tiny debug model in GCS

--- rabbitmq-support/src/main/java/de/unijena/bioinf/rabbitmq/RabbitUtils.java
+++ rabbitmq-support/src/main/java/de/unijena/bioinf/rabbitmq/RabbitUtils.java
@@ -21,6 +21,47 @@
 package de.unijena.bioinf.rabbitmq;
 
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import de.unijena.bioinf.ms.properties.PropertyManager;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class RabbitUtils {
+    public final static String JOB_EX = PropertyManager.getProperty("", null, "jobs");
+    public final static String DATA_EX = PropertyManager.getProperty("", null, "data");
+//    public final static String MANAGE_EX = PropertyManager.getProperty("", null, "manage");
+
+    public final static String SIRIUS_IN_EX = PropertyManager.getProperty("", null, "sirius.client.in");
+    public final static String SIRIUS_OUT_EX = PropertyManager.getProperty("", null, "sirius.client.out");
+
+    public final static String REGISTER_CLIENT_EX = PropertyManager.getProperty("", null, "client.register");
+    public final static String DEAD_JOB_EX = PropertyManager.getProperty("", null, "jobs.dead");
+    public final static String WORKER_EX = PropertyManager.getProperty("", null, "worker");
+
+
+    public static void ensureBaseInfrastructure(Channel channel) throws IOException {
+        //Alternative queue to manage clients
+        channel.exchangeDeclare(REGISTER_CLIENT_EX, BuiltinExchangeType.FANOUT, true, false, true, null);
+        channel.queueDeclare(REGISTER_CLIENT_EX + ".q", true, false, false, null);
+        channel.queueBind(REGISTER_CLIENT_EX + ".q", REGISTER_CLIENT_EX, "");
+
+        //dead letter queue for dead job messages
+        channel.exchangeDeclare(DEAD_JOB_EX, BuiltinExchangeType.FANOUT, true, false, true, null);
+        channel.queueDeclare(DEAD_JOB_EX + ".q", true, false, false, null);
+        channel.queueBind(DEAD_JOB_EX + ".q", DEAD_JOB_EX, "");
+
+        channel.exchangeDeclare(WORKER_EX, BuiltinExchangeType.TOPIC, true, false, false, null);
+
+        channel.exchangeDeclare(JOB_EX, BuiltinExchangeType.DIRECT, true, false, false, Map.of("alternate-exchange", REGISTER_CLIENT_EX));
+        channel.exchangeDeclare(DATA_EX, BuiltinExchangeType.DIRECT, true, false, false, null);
+
+        channel.exchangeDeclare(SIRIUS_IN_EX, BuiltinExchangeType.TOPIC, true, false, false, null);
+        channel.exchangeDeclare(SIRIUS_OUT_EX, BuiltinExchangeType.TOPIC, true, false, false, null);
+
+        channel.exchangeBind(DATA_EX, SIRIUS_IN_EX, "data.#");
+        channel.exchangeBind(JOB_EX, SIRIUS_IN_EX, "worker.#");
+    }
 
 }
