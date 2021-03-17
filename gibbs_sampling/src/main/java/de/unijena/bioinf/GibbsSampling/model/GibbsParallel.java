b3e058cf1075e5353edace556904b35b0a5159fa
fleisch <markus.fleischauer@uni-jena.de>
2021-03-17 14:42:57 +0100

    Optimized job cancellation and subjob lifecycle handling. improved job information in job manager panel

--- gibbs_sampling/src/main/java/de/unijena/bioinf/GibbsSampling/model/GibbsParallel.java
+++ gibbs_sampling/src/main/java/de/unijena/bioinf/GibbsSampling/model/GibbsParallel.java
@@ -145,20 +145,24 @@ public class GibbsParallel<C extends Candidate<?>> extends BasicMasterJJob<Compo
         updateProgress(0, maxProgress, 0, "Sample probabilities");
         List<BasicJJob> jobs = new ArrayList<>();
         for (final GibbsMFCorrectionNetwork gibbsNetwork : gibbsNetworks) {
+            checkForInterruption();
             gibbsNetwork.setIterationSteps(maxStepProportioned, burnIn);
             gibbsNetwork.addJobProgressListener(this);
             jobs.add(gibbsNetwork);
             submitSubJob(gibbsNetwork);
         }
 
+        checkForInterruption();
+
         for (BasicJJob job : jobs) {
             job.awaitResult();
         }
 
-
         long start = System.currentTimeMillis();
         combineResults();
         logDebug("combined all results in: "+(System.currentTimeMillis()-start)+" ms");
+
+        checkForInterruption();
 
         return createCompoundResults();
 
