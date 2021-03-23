2c1a11f80ae22aec777e862310d9a076a0092c02
fleisch <markus.fleischauer@uni-jena.de>
2021-03-23 16:32:51 +0100

    fixed memory leak in FormulaSummaryWriter, fixed inconsistent ranking of global compound summary, added formula rank to global summary.

--- sirius_cli/src/main/java/de/unijena/bioinf/projectspace/summaries/FormulaSummaryWriter.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/projectspace/summaries/FormulaSummaryWriter.java
@@ -42,6 +42,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
@@ -51,9 +53,9 @@ public class FormulaSummaryWriter implements Summarizer {
     final static List<Class<? extends FormulaScore>> RANKING_SCORES = List.of(ZodiacScore.class, SiriusScore.class, TreeScore.class, IsotopeScore.class, TopCSIScore.class);
     final static List<Class<? extends FormulaScore>> RANKING_SCORES_SELECTING_TOP1 = List.of(ZodiacScore.class, SiriusScore.class, TreeScore.class, IsotopeScore.class);
     final LinkedHashMap<Class<? extends FormulaScore>, String> globalTypes = new LinkedHashMap<>();
-    final Map<FormulaResult, Class<? extends FormulaScore>> globalResults = new HashMap<>();
-    final Map<FormulaResult, Class<? extends FormulaScore>> globalResultsAllAdducts = new HashMap<>();
-    final Map<FormulaResult, String> prefix = new HashMap<>();
+
+    final Set<ResultEntry> globalResults = new HashSet<>();
+    final Set<ResultEntry> globalResultsAllAdducts = new HashSet<>();
 
     @Override
     public List<Class<? extends DataAnnotation>> requiredFormulaResultAnnotations() {
@@ -84,8 +86,8 @@ public class FormulaSummaryWriter implements Summarizer {
                         .ifPresent(s -> {
                             if (first.getAndSet(false)) {
                                 FormulaResult bestResult = topResultWithAdducts.size()>1 ? resolveIonizationOnly(r.getCandidate()) : r.getCandidate();
-                                this.globalResults.put(bestResult, r.getScoreObject().getClass());
-                                this.prefix.put(bestResult, exp.getId().getIonMass().orElse(Double.NaN) + "\t" + exp.getId().getRt().orElse(RetentionTime.NA()).getRetentionTimeInSeconds() + "\t" + exp.getId().getDirectoryName());
+                                this.globalResults.add(ResultEntry.of(bestResult,exp));
+//                                this.prefix.put(bestResult, exp.getId().getIonMass().orElse(Double.NaN) + "\t" + exp.getId().getRt().orElse(RetentionTime.NA()).getRetentionTimeInSeconds() + "\t" + exp.getId().getDirectoryName());
                             }
                             s.annotations().forEach((key, value) -> {
                                 if (value != null && !value.isNa()) {
@@ -98,23 +100,20 @@ public class FormulaSummaryWriter implements Summarizer {
                 //writing stuff
                 types.remove(TopCSIScore.class);
                 types.remove(ConfidenceScore.class);
-                writeCSV(w, types, results, null);
+                writeCSV(w, types, results.stream().map(s -> new SScored<>(ResultEntry.of(s.getCandidate(),exp),s.getScoreObject())).collect(Collectors.toList()), false);
             });
 
             return true;
         });
 
-        topResultWithAdducts.forEach(r -> {
-            this.globalResultsAllAdducts.put(r.getCandidate(), r.getScoreObject().getClass());
-            this.prefix.put(r.getCandidate(), exp.getId().getIonMass().orElse(Double.NaN) + "\t" + exp.getId().getRt().orElse(RetentionTime.NA()).getRetentionTimeInSeconds() + "\t" + exp.getId().getDirectoryName());
-        });
+        topResultWithAdducts.forEach(r -> this.globalResultsAllAdducts.add(ResultEntry.of(r.getCandidate(), exp)));
     }
 
     private FormulaResult resolveIonizationOnly(FormulaResult r) {
         FormulaResultId rid = r.getId();
         FormulaResultId newRid = new FormulaResultId(rid.getParentId(), rid.getPrecursorFormula(), PrecursorIonType.getPrecursorIonType(rid.getIonType().getIonization()));
         FormulaResult newResult = new FormulaResult(newRid);
-        r.annotations().forEach((k,v)-> newResult.setAnnotation(k, v));
+        r.annotations().forEach(newResult::setAnnotation);
         return newResult;
     }
 
@@ -141,13 +140,13 @@ public class FormulaSummaryWriter implements Summarizer {
         globalTypes.remove(ConfidenceScore.class);
         globalTypes.remove(TopCSIScore.class);
 
-        final List<SScored<FormulaResult, ? extends FormulaScore>> r = FormulaScoring.rankBy(globalResults.keySet(), RANKING_SCORES, true);
+        final List<SScored<ResultEntry, ? extends FormulaScore>> r = FormulaScoring.rankBy(globalResults.stream(), RANKING_SCORES, true, ResultEntry::getScoring);
         writer.textFile(SummaryLocations.FORMULA_SUMMARY, w -> {
-            writeCSV(w, globalTypes, r, prefix);
+            writeCSV(w, globalTypes, r, true);
         });
-        final List<SScored<FormulaResult, ? extends FormulaScore>> rAdducts = FormulaScoring.rankBy(globalResultsAllAdducts.keySet(), RANKING_SCORES, true);
+        final List<SScored<ResultEntry, ? extends FormulaScore>> rAdducts = FormulaScoring.rankBy(globalResultsAllAdducts.stream(), RANKING_SCORES, true, ResultEntry::getScoring);
         writer.textFile(SummaryLocations.FORMULA_SUMMARY_ADDUCTS, w -> {
-            writeCSV(w, globalTypes, rAdducts, prefix);
+            writeCSV(w, globalTypes, rAdducts, true);
         });
     }
 
@@ -159,66 +158,108 @@ public class FormulaSummaryWriter implements Summarizer {
         return headerBuilder.toString();
     }
 
-    private void writeCSV(Writer w, LinkedHashMap<Class<? extends FormulaScore>, String> types, List<? extends SScored<? extends FormulaResult, ? extends Score<?>>> results, Map<FormulaResult, String> prefix) throws IOException {
+    private void writeCSV(Writer w, LinkedHashMap<Class<? extends FormulaScore>, String> types, List<? extends SScored<? extends ResultEntry, ? extends Score<?>>> results, boolean prefix) throws IOException {
         final List<Class<? extends FormulaScore>> scoreOrder = ProjectSpaceManager.scorePriorities().stream().filter(types::containsKey).collect(Collectors.toList());
         results = results.stream()
                 .sorted((i1, i2) -> FormulaScoring.comparingMultiScore(scoreOrder).compare(
-                        i1.getCandidate().getAnnotationOrThrow(FormulaScoring.class),
-                        i2.getCandidate().getAnnotationOrThrow(FormulaScoring.class)))
+                        i1.getCandidate().scoring,
+                        i2.getCandidate().scoring))
                 .collect(Collectors.toList());
 
 
         String header = makeHeader(scoreOrder.stream().map(types::get).collect(Collectors.joining("\t")));
-        if (prefix != null)
+        if (prefix)
             header = header + "\tionMass" + "\tretentionTimeInSeconds" + "\tid";
 
         w.write("rank\t" + header + "\n");
 
         int rank = 0;
         MolecularFormula preFormula = null;
-        for (SScored<? extends FormulaResult, ? extends Score<?>> s : results) {
-            FormulaResult r = s.getCandidate();
-            PrecursorIonType ion = r.getId().getIonType();
-            FormulaScoring scores = r.getAnnotationOrThrow(FormulaScoring.class);
-            FTree tree = r.getAnnotationOrNull(FTree.class);
-            if (preFormula == null || !r.getId().getPrecursorFormula().equals(preFormula))
+        for (SScored<? extends ResultEntry, ? extends Score<?>> s : results) {
+            ResultEntry r = s.getCandidate();
+            FormulaScoring scores = r.scoring;
+            if (preFormula == null || !r.preFormula.equals(preFormula))
                 rank++;
-            preFormula = r.getId().getPrecursorFormula();
+            preFormula = r.preFormula;
 
 
             w.write(String.valueOf(rank));
             w.write('\t');
-            w.write(r.getId().getMolecularFormula().toString());
+            w.write(r.molecularFormula.toString());
             w.write('\t');
-            w.write(ion != null ? ion.toString() : "?");
+            w.write(r.ion);
             w.write('\t');
 
             w.write(preFormula.toString());
             w.write('\t');
 
-
-//            w.write(s.getScoreObject().toString());
-//            w.write('\t');
-            //writing different Scores to file e.g. sirius and zodiac
             for (Class<? extends FormulaScore> k : scoreOrder) {
                 w.write(scores.getAnnotationOr(k, FormulaScore::NA).toString());
                 w.write('\t');
             }
-            w.write(tree != null ? String.valueOf(tree.numberOfVertices()) : "");
+            w.write(r.treeNumVertices);
             w.write('\t');
-            w.write(tree != null ? String.valueOf(tree.getAnnotationOrThrow(TreeStatistics.class).getExplainedIntensity()) : "");
+            w.write(r.treeExplInt);
             w.write('\t');
-            w.write(tree != null ? String.valueOf(new FTreeMetricsHelper(tree).getMedianMassDeviation().getPpm()) : "");
+            w.write(r.medianMassDev);
             w.write('\t');
-            w.write(tree != null ? String.valueOf(new FTreeMetricsHelper(tree).getMedianAbsoluteMassDeviation().getPpm()) : "");
+            w.write(r.medianAbsMassDev);
             w.write('\t');
-            w.write(tree != null ? r.getId().getParentId().getIonMass().map(e -> tree.getMassErrorTo(tree.getRoot(), e).getPpm()).map(String::valueOf).orElse("N/A") : "");
-            if (prefix != null) {
+            w.write(r.massErrorPrecursor);
+            if (prefix) {
                 w.write('\t');
-                w.write(prefix.get(r));
+                w.write(r.ionMass);
+                w.write('\t');
+                w.write(r.retentionTimeSeconds);
+                w.write('\t');
+                w.write(r.dirName);
             }
 
             w.write('\n');
+        }
+    }
+
+    private static class ResultEntry {
+        public final FormulaScoring scoring;
+        public final MolecularFormula molecularFormula;
+        public final MolecularFormula preFormula;
+        public final String ion;
+        private final String ionMass;
+        private final String retentionTimeSeconds;
+        private final String dirName;
+
+        public String treeNumVertices = "N/A";
+        public String treeExplInt = "N/A";
+        public String medianMassDev = "N/A";
+        public String medianAbsMassDev = "N/A";
+        public String massErrorPrecursor = "N/A";
+
+        public ResultEntry(FormulaResult r, CompoundContainer exp) {
+            scoring = r.getAnnotationOrThrow(FormulaScoring.class);
+            molecularFormula = r.getId().getMolecularFormula();
+            preFormula = r.getId().getPrecursorFormula();
+            ion = Optional.ofNullable(r.getId().getIonType()).map(PrecursorIonType::toString).orElse("N/A");
+
+            //tree stuff
+            r.getAnnotation(FTree.class).ifPresent(tree -> {
+                treeNumVertices = String.valueOf(tree.numberOfVertices());
+                treeExplInt = String.valueOf(tree.getAnnotationOrThrow(TreeStatistics.class).getExplainedIntensity());
+                medianMassDev = String.valueOf(new FTreeMetricsHelper(tree).getMedianMassDeviation().getPpm());
+                medianAbsMassDev = String.valueOf(new FTreeMetricsHelper(tree).getMedianAbsoluteMassDeviation().getPpm());
+                massErrorPrecursor = r.getId().getParentId().getIonMass().map(e -> tree.getMassErrorTo(tree.getRoot(), e).getPpm()).map(String::valueOf).orElse("N/A");
+            });
+
+            ionMass = BigDecimal.valueOf(exp.getId().getIonMass().orElse(Double.NaN)).setScale(5, RoundingMode.HALF_UP).toString();
+            retentionTimeSeconds = String.valueOf(exp.getId().getRt().orElse(RetentionTime.NA()).getRetentionTimeInSeconds());
+            dirName = exp.getId().getDirectoryName();
+        }
+
+        public FormulaScoring getScoring() {
+            return scoring;
+        }
+
+        public static ResultEntry of(@NotNull FormulaResult r, @NotNull CompoundContainer exp){
+            return new ResultEntry(r, exp);
         }
     }
 }
