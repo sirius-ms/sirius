879860a2e19d6c5b4d72b506be0df7cab3cf6872
fleisch <markus.fleischauer@uni-jena.de>
2021-04-30 02:08:41 +0200

    added AmpqClient and prepared SIRIUS for its usage

--- sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/SiriusCLIApplication.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/SiriusCLIApplication.java
@@ -89,10 +89,7 @@ public class SiriusCLIApplication {
     public static Runnable shutdownWebservice() {
         return () -> {
             try {
-                ApplicationCore.WEB_API.shutdownJobWatcher();
-                ApplicationCore.DEFAULT_LOGGER.info("Try to delete leftover jobs on web server...");
-                ApplicationCore.WEB_API.deleteClientAndJobs();
-                ApplicationCore.DEFAULT_LOGGER.info("...Job deletion Done!");
+                ApplicationCore.WEB_API.shutdown();
             } catch (IOException e) {
                 e.printStackTrace();
             }
