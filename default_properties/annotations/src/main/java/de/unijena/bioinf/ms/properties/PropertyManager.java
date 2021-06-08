81c6030312916ab2ba4a722b5be2a3085560b8a9
fleisch <markus.fleischauer@uni-jena.de>
2021-06-08 23:39:48 +0200

    running worker messaging

--- default_properties/annotations/src/main/java/de/unijena/bioinf/ms/properties/PropertyManager.java
+++ default_properties/annotations/src/main/java/de/unijena/bioinf/ms/properties/PropertyManager.java
@@ -42,6 +42,7 @@ import java.util.LinkedHashSet;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 /**
@@ -80,7 +81,11 @@ public class PropertyManager {
             PROPERTIES.addConfiguration(PERSISTENT_PROPERTIES, "PERSISTENT_PROPERTIES");
             PERSISTENT_PROPERTIES.addEventListener(CombinedConfiguration.COMBINED_INVALIDATE, event -> PROPERTIES.invalidate());
             CHANGED_PROPERTIES = loadDefaultProperties();
-            Reflections.log.ifPresent(l -> LogManager.getLogManager().getLogger(l.getName()).setLevel(Level.SEVERE));
+            Reflections.log.ifPresent(l -> {
+                Logger logger = LogManager.getLogManager().getLogger(l.getName());
+                if (logger != null)
+                    logger.setLevel(Level.SEVERE);
+            });
             final Reflections reflections = new Reflections("de.unijena.bioinf.ms.defaults", new ResourcesScanner());
             DEFAULTS_LAYOUT = new PropertiesConfigurationLayout();
 
--- web_service_oss/amqp_client_oss/src/main/java/de/unijena/bioinf/ms/amqp/client/AmqpClients.java
+++ web_service_oss/amqp_client_oss/src/main/java/de/unijena/bioinf/ms/amqp/client/AmqpClients.java
@@ -17,46 +17,23 @@
  *
  *  You should have received a copy of the GNU Lesser General Public License along with SIRIUS. If not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
  */
-
-/*
- *
- *  This file is part of the SIRIUS library for analyzing MS and MS/MS data
- *
- *  Copyright (C) 2013-2020 Kai Dührkop, Markus Fleischauer, Marcus Ludwig, Martin A. Hoffman, Fleming Kretschmer and Sebastian Böcker,
- *  Chair of Bioinformatics, Friedrich-Schilller University.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License along with SIRIUS. If not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
- */
 package de.unijena.bioinf.ms.amqp.client;
 
 import de.unijena.bioinf.ms.properties.PropertyManager;
 
 public class AmqpClients {
 
-    protected static final String QUEUE_CATEGORY = PropertyManager.getProperty("", null, "worker");
-    protected static final String WORKER_QUEUE_TYPE = PropertyManager.getProperty("", null, "job");
-    protected static final String WORKER_SUFFIX = PropertyManager.getProperty("", null, "ce");
+    protected static final String WORKER_QUEUE_CATEGORY = PropertyManager.getProperty("de.unijena.bioinf.sirius.amqp.type.worker", null, "worker");
+    protected static final String WORKER_QUEUE_TYPE = PropertyManager.getProperty("de.unijena.bioinf.sirius.amqp.type.job", null, "job");
+    protected static final String WORKER_SUFFIX = PropertyManager.getProperty("de.unijena.bioinf.sirius.amqp.suffix.worker", null, "ce");
+    protected static final String DATA_QUEUE_CATEGORY = PropertyManager.getProperty("de.unijena.bioinf.sirius.amqp.type.data", null, "data");
 
 
     public static String jobRoutePrefix(String jobType, boolean pos) {
-        return QUEUE_CATEGORY + "." + jobType + "." + (pos ? "pos" : "neg") + "." + WORKER_SUFFIX + "." + WORKER_QUEUE_TYPE;
+        return WORKER_QUEUE_CATEGORY + "." + jobType + "." + (pos ? "pos" : "neg") + "." + WORKER_SUFFIX + "." + WORKER_QUEUE_TYPE;
     }
 
     public static String dataRoutingKeyPrefix(String data, boolean pos) {
-        return null; //todo
-    }
-
-    public static String modelRoutingKeyPrefix(String model, boolean pos) {
-        return null; //todo
+        return DATA_QUEUE_CATEGORY + ".client";
     }
 }
