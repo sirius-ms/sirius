0ae3d7b14509b9bae572a4c69a250d33231b8a9f
ju52les <nils.alexander.haupt@uni-jena.de>
2021-12-29 19:20:07 +0100

    Implementation of RemoveDanglingSubtrees

--- combinatorial_fragmenter/src/main/java/de/unijena/bioinf/fragmenter/CombinatorialSubtreeManipulator.java
+++ combinatorial_fragmenter/src/main/java/de/unijena/bioinf/fragmenter/CombinatorialSubtreeManipulator.java
@@ -1,9 +1,33 @@
 package de.unijena.bioinf.fragmenter;
 
+import gnu.trove.map.hash.TObjectIntHashMap;
+
 public class CombinatorialSubtreeManipulator {
 
-    // TODO
-    public static void removeDanglingSubtrees(CombinatorialSubtree tree){
+    public static double removeDanglingSubtrees(CombinatorialSubtree tree){
+        // 1. Initialisation
+        TObjectIntHashMap<CombinatorialNode> indices = new TObjectIntHashMap<>();
+        int idx = 0;
+        for(CombinatorialNode node : tree){
+            indices.put(node,idx);
+            idx++;
+        }
+        // 2. On-the-fly removal of subtrees with negative profit:
+        return getBestSubtreeScore(tree.getRoot(), tree, indices);
+    }
 
+    private static double getBestSubtreeScore(CombinatorialNode currentNode, CombinatorialSubtree tree, TObjectIntHashMap<CombinatorialNode> indices){
+        double score = 0;
+        for(CombinatorialEdge edge : currentNode.getOutgoingEdges()){
+            CombinatorialNode child = edge.target;
+            double childScore = getBestSubtreeScore(child, tree, indices) + child.fragmentScore + edge.score;
+
+            if(childScore < 0){ // retaining the subtree below 'child' results in a smaller profit of this whole tree
+                tree.removeSubtree(child.fragment);
+            }else{
+                score = score + childScore;
+            }
+        }
+        return score;
     }
 }
