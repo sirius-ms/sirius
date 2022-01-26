59577756b65e22ed5cf54b993aedc97002a39b39
ju52les <nils.alexander.haupt@uni-jena.de>
2022-01-26 03:02:54 +0100

    Changed 'computeMapping' function in CombinatorialSubtreeCalculator

--- combinatorial_fragmenter/src/main/java/de/unijena/bioinf/fragmenter/CombinatorialSubtreeCalculator.java
+++ combinatorial_fragmenter/src/main/java/de/unijena/bioinf/fragmenter/CombinatorialSubtreeCalculator.java
@@ -1,9 +1,12 @@
 package de.unijena.bioinf.fragmenter;
 
+import de.unijena.bioinf.ChemistryBase.algorithm.BitsetOps;
+import de.unijena.bioinf.ChemistryBase.chem.MolecularFormula;
 import de.unijena.bioinf.ChemistryBase.ms.ft.FTree;
 import de.unijena.bioinf.ChemistryBase.ms.ft.Fragment;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.HashMap;
 
 public abstract class CombinatorialSubtreeCalculator extends AbstractFragmentationTreeAnnotator {
@@ -25,37 +28,52 @@ public abstract class CombinatorialSubtreeCalculator extends AbstractFragmentati
 
     public abstract CombinatorialSubtree computeSubtree() throws Exception;
 
-    /*
-     * Compute the mapping by using the calculated subtree.
-     * Each node (Fragment object) in the FTree is assigned a list of CombinatorialFragments.
-     * If there is a molecular fragment in the subtree with a molecular formula which is also contained in the FTree,
-     * this fragment is added to the corresponding FTree node list.
-     */
-    /* TODO: fragmentation without hydrogens but mapping and adding terminal nodes with hydrogens
-             --> because removing hydrogens causes a potential lose in information
-             --> e.g. C2H6 for ethan and C2H4 for ethen
+    /**
+     * Returns a mapping which assigns each {@link Fragment} (or molecular formula) contained in the fragmentation tree
+     * a {@link CombinatorialFragment} whose molecular formula differs only in the number the hydrogen atoms compared to
+     * {@link Fragment#getFormula()},
+     * and this {@link CombinatorialFragment} is contained in the computed {@link CombinatorialSubtree}.
+     *
+     * @return a mapping which assigns each {@link Fragment} in {@link FTree} a {@link CombinatorialFragment} contained in the computed subtree
      */
     @Override
     public HashMap<Fragment, ArrayList<CombinatorialFragment>> computeMapping(){
-        // 1.) Compute a HashMap which maps each Molecular Formula the corresponding Fragment of the FTree.
-        // 2.) At the same time, the mapping, which maps each Fragment to a List of CombinatorialFragments, can be initialized.
-        HashMap<String, Fragment> mf2FtFrag = new HashMap<>();
-        for(Fragment ftFrag : this.fTree){
-            String mf = ftFrag.getFormula().toString();
-            mf2FtFrag.put(mf, ftFrag);
-            this.mapping.put(ftFrag, new ArrayList<CombinatorialFragment>());
+        // 1. Create a mapping which maps the molecular formula of each terminal node to the corresponding fragment which
+        // is connected to it.
+        HashMap<MolecularFormula, CombinatorialFragment> mf2Frag = new HashMap<>();
+        BitSet bitset = new BitSet();
+        bitset.set(this.getMolecule().natoms); //'bitset' represent the bitset of a terminal node with number 0
+
+        for(int i = 0; i < this.fTree.numberOfVertices(); i++){ // #terminal nodes <= #vertices in fTree
+            CombinatorialNode terminalNode = this.subtree.getNode(bitset);
+            if(terminalNode != null){
+                CombinatorialFragment fragment = terminalNode.incomingEdges.get(0).source.fragment;
+                mf2Frag.put(terminalNode.fragment.getFormula(),fragment);
+            }
+            this.incrementBitSet(bitset);
         }
 
-        // 3.) Compute the mapping:
-        for(CombinatorialNode node : this.subtree){
-            String fragmentMf = node.fragment.getFormula().toString();
-            Fragment ftFrag = mf2FtFrag.get(fragmentMf);
-            if(ftFrag != null){
-                this.mapping.get(ftFrag).add(node.fragment);
+        // 2. Iterate through this.fTree and for each vertex (Fragment) get the corresponding CombinatorialFragment
+        // by using 'mf2Frag' and put it into the hashmap.
+        for(Fragment ftFrag : this.fTree){
+            CombinatorialFragment fragment = mf2Frag.get(ftFrag.getFormula());
+            if(fragment != null){
+                ArrayList<CombinatorialFragment> fragList = new ArrayList<>();
+                fragList.add(fragment);
+                this.mapping.put(ftFrag, fragList);
             }
         }
 
         return this.mapping;
+    }
+
+    private void incrementBitSet(BitSet bitSet){
+        int idx = 0;
+        while(bitSet.get(idx)){
+            bitSet.set(idx, false);
+            idx++;
+        }
+        bitSet.set(idx);
     }
 
     public CombinatorialSubtree getSubtree(){
