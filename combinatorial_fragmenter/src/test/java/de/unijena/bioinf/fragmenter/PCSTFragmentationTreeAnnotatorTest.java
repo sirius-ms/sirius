1e7663c9d4a7327b3fd3a051c1df633685683e50
ju52les <nils.alexander.haupt@uni-jena.de>
2022-02-07 02:58:27 +0100

    JUnit Test for ILP

--- combinatorial_fragmenter/src/test/java/de/unijena/bioinf/fragmenter/PCSTFragmentationTreeAnnotatorTest.java
+++ combinatorial_fragmenter/src/test/java/de/unijena/bioinf/fragmenter/PCSTFragmentationTreeAnnotatorTest.java
@@ -1,48 +1,62 @@
 package de.unijena.bioinf.fragmenter;
 
+import de.unijena.bioinf.ChemistryBase.chem.ElectronIonization;
+import de.unijena.bioinf.ChemistryBase.chem.MolecularFormula;
+import de.unijena.bioinf.ChemistryBase.chem.utils.UnknownElementException;
+import de.unijena.bioinf.ChemistryBase.ms.ft.FTree;
+import de.unijena.bioinf.ChemistryBase.ms.ft.Fragment;
 import gurobi.GRBException;
+import org.junit.Test;
 import org.openscience.cdk.exception.InvalidSmilesException;
-import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+
+import static org.junit.Assert.assertEquals;
 
 public class PCSTFragmentationTreeAnnotatorTest {
 
-    public static void main(String[] args){
-        try {
-            String smiles = "C1CC1";
+    private static CombinatorialFragmenterScoring DEFAULT_SCORING = new CombinatorialFragmenterScoring() {
+        @Override
+        public double scoreBond(IBond bond, boolean direction) {
+            return -1d;
+        }
+
+        @Override
+        public double scoreFragment(CombinatorialNode fragment) {
+            return 0;
+        }
+
+        @Override
+        public double scoreEdge(CombinatorialEdge edge) {
+            if(edge.target.fragment.isRealFragment()){
+                return this.scoreBond(edge.getCut1(), edge.getDirectionOfFirstCut()) + (edge.getCut2() != null ? scoreBond(edge.getCut2(), edge.getDirectionOfSecondCut()) : 0);
+            }else{
+                return 10 - Math.abs(edge.source.fragment.hydrogenRearrangements(edge.target.fragment.getFormula()));
+            }
+        }
+    };
+
+    @Test
+    public void testCycloButadiene(){
+        try{
+            String smiles = "C1=CC=C1";
             SmilesParser parser = new SmilesParser(SilentChemObjectBuilder.getInstance());
-            IAtomContainer molContainer = parser.parseSmiles(smiles);
-            AtomContainerManipulator.convertImplicitToExplicitHydrogens(molContainer);
-            MolecularGraph molecule = new MolecularGraph(molContainer);
+            MolecularGraph molecule = new MolecularGraph(parser.parseSmiles(smiles));
 
-            CombinatorialFragmenterScoring scoring = new CombinatorialFragmenterScoring() {
-                @Override
-                public double scoreBond(IBond bond, boolean direction) {
-                    return 1;
-                }
+            FTree fTree = new FTree(MolecularFormula.parse("C4H4"), new ElectronIonization());
+            fTree.addFragment(fTree.getRoot(), new Fragment(1, MolecularFormula.parse("C2H2"), new ElectronIonization()));
+            fTree.addFragment(fTree.getRoot(), new Fragment(2, MolecularFormula.parse("CH2"), new ElectronIonization()));
 
-                @Override
-                public double scoreFragment(CombinatorialNode fragment) {
-                    return 1;
-                }
+            PCSTFragmentationTreeAnnotator subtreeCalc = new PCSTFragmentationTreeAnnotator(fTree, molecule, DEFAULT_SCORING);
+            subtreeCalc.initialize(n -> true);
+            CombinatorialSubtree subtree = subtreeCalc.computeSubtree();
 
-                @Override
-                public double scoreEdge(CombinatorialEdge edge) {
-                    return edge.cut2 == null ? 1 : 2;
-                }
-            };
-
-            PCSTFragmentationTreeAnnotator annotator = new PCSTFragmentationTreeAnnotator(null, molecule, scoring);
-            annotator.initialize(n -> true);
-            CombinatorialSubtree subtree = annotator.computeSubtree();
-            System.out.println(subtree.toString());
-            System.out.println(subtree.getScore());
-
-        } catch (InvalidSmilesException | GRBException e) {
+            assertEquals(26.0, subtreeCalc.getScore(), 0.0);
+            assertEquals(26.0, subtree.getScore(), 0.0);
+        }catch(UnknownElementException | InvalidSmilesException | GRBException e){
             e.printStackTrace();
         }
     }
+
 }
