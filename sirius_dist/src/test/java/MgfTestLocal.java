e26e445f09b90768bf375e8bf153ae7ea3a38c4b
joXem <jonas.emmert@uni-jena.de>
2022-08-19 16:15:38 +0200

    included service_single_os into pipeline and local testing

--- sirius_dist/src/test/java/MgfTestLocal.java
+++ sirius_dist/src/test/java/MgfTestLocal.java
@@ -13,29 +13,29 @@ public class MgfTestLocal {
     @Test
     @DisplayName("Testing if SIRIUS calculates expected formula candidates with mgf file.")
     public void testTopCandidates() throws IOException {
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus_npc".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/fingerid".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/fingerprints".replace("/", sep));
+        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus".replace("/", sep));
+        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus_npc".replace("/", sep));
+        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/fingerid".replace("/", sep));
+        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/fingerprints".replace("/", sep));
 
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus_npc".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/fingerid".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/fingerprints".replace("/", sep)));
+        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus".replace("/", sep)));
+        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus_npc".replace("/", sep)));
+        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/fingerid".replace("/", sep)));
+        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/fingerprints".replace("/", sep)));
 
-        TestMethods.areContentsEqual(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus".replace("/", sep),
-                absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/canopus_npc".replace("/", sep),
-                absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/0_laudanosine_FEATURE_1/fingerprints".replace("/", sep));
+        TestMethods.areContentsEqual(absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus".replace("/", sep),
+                absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/canopus_npc".replace("/", sep),
+                absPath + "sirius_cli/src/test/temp_results/mgf_temp_output/1_laudanosine_FEATURE_1/fingerprints".replace("/", sep));
 
         int rank_count = 3;
         int table_feature = 1;
         String[] pre_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/test_results/mgf_candidates/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
-        String[] post_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/mgf_temp_summary/0_laudanosine_FEATURE_1/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
+        String[] post_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/mgf_temp_summary/1_laudanosine_FEATURE_1/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
 
         rank_count = 3;
         table_feature = 6;
         String[] pre_structure = TestMethods.readCandidates(absPath + "sirius_cli/src/test/test_results/mgf_candidates/structure_candidates.tsv".replace("/", sep), rank_count, table_feature);
-        String[] post_structure = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/mgf_temp_summary/0_laudanosine_FEATURE_1/structure_candidates.tsv".replace("/", sep), rank_count, table_feature);
+        String[] post_structure = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/mgf_temp_summary/1_laudanosine_FEATURE_1/structure_candidates.tsv".replace("/", sep), rank_count, table_feature);
 
         assertArrayEquals(pre_formula, post_formula);
         assertArrayEquals(pre_structure, post_structure);
--- sirius_dist/src/test/java/TxtTestLocal.java
+++ sirius_dist/src/test/java/TxtTestLocal.java
@@ -13,7 +13,7 @@ public class TxtTestLocal {
         int rank_count = 3;
         int table_feature = 1;
         String[] pre_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/test_results/txt_candidates/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
-        String[] post_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/txt_temp_summary/0_unknown_/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
+        String[] post_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/txt_temp_summary/1_unknown_/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
 
         assertArrayEquals(pre_formula, post_formula);
         System.out.println("TXT passed");
