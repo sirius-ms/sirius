055b9f39e89cfe6c2cc52f3f9eb99769d9f802c8
joXem <jonas.emmert@uni-jena.de>
2022-08-21 16:36:18 +0200

    cleaner ms and mgf test classes [ci-skip]

--- sirius_dist/src/test/java/MsTestLocal.java
+++ sirius_dist/src/test/java/MsTestLocal.java
@@ -10,34 +10,34 @@ public class MsTestLocal {
 
     private static final String absPath = System.getProperty("user.dir").split("sirius_dist")[0];
     private static final String sep = System.getProperty("file.separator");
+    private static final String temp_output = absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/".replace("/", sep);
+    private static final String temp_summary = absPath + "sirius_cli/src/test/temp_results/ms_temp_summary/1_Bicuculline_Bicuculline/".replace("/", sep);
 
     @Test
     @DisplayName("Testing if SIRIUS calculates expected formula candidates with ms file.")
     public void testTopCandidates() throws IOException {
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus_npc".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/fingerid".replace("/", sep));
-        TestMethods.isDirExisting(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/fingerprints".replace("/", sep));
+        TestMethods.isDirExisting(temp_output + "canopus");
+        TestMethods.isDirExisting(temp_output + "canopus_npc");
+        TestMethods.isDirExisting(temp_output + "fingerid");
+        TestMethods.isDirExisting(temp_output + "fingerprints");
 
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus_npc".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/fingerid".replace("/", sep)));
-        TestMethods.isDirNotEmpty(Paths.get(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/fingerprints".replace("/", sep)));
+        TestMethods.isDirNotEmpty(Paths.get(temp_output + "canopus"));
+        TestMethods.isDirNotEmpty(Paths.get(temp_output + "canopus_npc"));
+        TestMethods.isDirNotEmpty(Paths.get(temp_output + "fingerid"));
+        TestMethods.isDirNotEmpty(Paths.get(temp_output + "fingerprints"));
 
-        TestMethods.areContentsEqual(absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus".replace("/", sep),
-                absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/canopus_npc".replace("/", sep),
-                absPath + "sirius_cli/src/test/temp_results/ms_temp_output/1_Bicuculline_Bicuculline/fingerprints".replace("/", sep));
+        TestMethods.areContentsEqual(temp_output + "canopus", temp_output + "canopus_npc", temp_output + "fingerprints");
 
 
         int rank_count = 3;
         int table_feature = 2;
         String[] pre_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/test_results/ms_candidates/ignore_formula/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
-        String[] post_formula = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/ms_temp_summary/1_Bicuculline_Bicuculline/formula_candidates.tsv".replace("/", sep), rank_count, table_feature);
+        String[] post_formula = TestMethods.readCandidates(temp_summary + "formula_candidates.tsv", rank_count, table_feature);
 
         rank_count = 3;
         table_feature = 6;
         String[] pre_structure = TestMethods.readCandidates(absPath + "sirius_cli/src/test/test_results/ms_candidates/compound_class_annotation/structure_candidates.tsv".replace("/", sep), rank_count, table_feature);
-        String[] post_structure = TestMethods.readCandidates(absPath + "sirius_cli/src/test/temp_results/ms_temp_summary/1_Bicuculline_Bicuculline/structure_candidates.tsv".replace("/", sep), rank_count, table_feature);
+        String[] post_structure = TestMethods.readCandidates(temp_summary + "structure_candidates.tsv", rank_count, table_feature);
 
         assertArrayEquals(pre_formula, post_formula);
         assertArrayEquals(pre_structure, post_structure);
