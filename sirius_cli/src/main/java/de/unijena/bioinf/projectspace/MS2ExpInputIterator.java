afb6c3f20c4fa73b5903d01c697f0688c04629d2
fleisch <markus.fleischauer@uni-jena.de>
2023-01-03 19:11:13 +0100

    enable msexperiment validation during import.

--- sirius_cli/src/main/java/de/unijena/bioinf/projectspace/MS2ExpInputIterator.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/projectspace/MS2ExpInputIterator.java
@@ -25,11 +25,14 @@ import de.unijena.bioinf.ChemistryBase.chem.Smiles;
 import de.unijena.bioinf.ChemistryBase.ms.Ms2Experiment;
 import de.unijena.bioinf.ChemistryBase.ms.MutableMs2Experiment;
 import de.unijena.bioinf.ChemistryBase.ms.Spectrum;
+import de.unijena.bioinf.ChemistryBase.ms.inputValidators.Warning;
 import de.unijena.bioinf.babelms.GenericParser;
 import de.unijena.bioinf.babelms.MsExperimentParser;
 import de.unijena.bioinf.jjobs.JobProgressMerger;
 import de.unijena.bioinf.jjobs.ProgressInputStream;
 import de.unijena.bioinf.sirius.Sirius;
+import de.unijena.bioinf.sirius.validation.Ms1Validator;
+import de.unijena.bioinf.sirius.validation.Ms2Validator;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -141,6 +144,11 @@ public class MS2ExpInputIterator implements InstIterProvider {
                             experiment.setMolecularFormula(null);
                             experiment.removeAnnotation(InChI.class);
                             experiment.removeAnnotation(Smiles.class);
+                        }
+                        if (experiment.getMs2Spectra().isEmpty()){
+                            new Ms1Validator().validate(experiment, Warning.Logger, true);
+                        } else {
+                            new Ms2Validator().validate(experiment, Warning.Logger, true);
                         }
                         instances.add(experiment);
                         return currentExperimentIterator;
