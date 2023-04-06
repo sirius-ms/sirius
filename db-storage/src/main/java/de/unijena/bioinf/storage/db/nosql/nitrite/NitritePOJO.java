b7e3a2a5ad8a4c9910751a9d225d3b7d497754f7
mel <martin.engler-lukajewski@bright-giant.com>
2023-04-06 11:47:47 +0200

    toString()

--- db-storage/src/main/java/de/unijena/bioinf/storage/db/nosql/nitrite/NitritePOJO.java
+++ db-storage/src/main/java/de/unijena/bioinf/storage/db/nosql/nitrite/NitritePOJO.java
@@ -41,4 +41,9 @@ public abstract class NitritePOJO extends POJO implements NitriteMappable, Nitri
         this.id = id;
     }
 
+    @Override
+    public String toString() {
+        return this.writeString();
+    }
+
 }
