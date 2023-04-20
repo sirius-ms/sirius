832c6ca08d7a38b939c80f815cfc76a3436316c2
ju52les <nils.alexander.haupt@uni-jena.de>
2023-04-21 00:12:16 +0200

    Added some stability to the code (e.g. ppm >= 1e6)

--- affinity_selection_ms/src/main/java/combinatorial_molecule_library_design/MassDeviationDependentBinDistribution.java
+++ affinity_selection_ms/src/main/java/combinatorial_molecule_library_design/MassDeviationDependentBinDistribution.java
@@ -30,7 +30,7 @@ public class MassDeviationDependentBinDistribution extends CMLDistribution{
         ArrayList<Integer> binEdges = new ArrayList<>();
         binEdges.add(minMoleculeMass);
 
-        double nextBinFactor = (1 + this.relDev) / (1 - this.relDev);
+        double nextBinFactor = this.relDev < 1 ? (1 + this.relDev) / (1 - this.relDev) : (1 + 2 * this.relDev); // stability
         int previousBinEdge = minMoleculeMass;
         int nextBinEdge = (int) (previousBinEdge * nextBinFactor);
         while(nextBinEdge <= maxMoleculeMass) {
@@ -47,7 +47,24 @@ public class MassDeviationDependentBinDistribution extends CMLDistribution{
         int numBins = binEdges.size() - 1;
         int diff = maxMoleculeMass - previousBinEdge;
         if(diff > 0){ // previousBinEdge < maxMoleculeMass and the bin widths have to be adjusted equally!
-            // todo: add the rest from above to bottom
+            int extensionForEachBin = diff / numBins;
+            int rest = diff - numBins * extensionForEachBin;
+
+            // Add extensionForEachBin to each bin:
+            if(extensionForEachBin > 0) {
+                for (int i = 1; i < binEdges.size(); i++) {
+                    int modifiedBinEdge = binEdges.get(i) + i * extensionForEachBin;
+                    binEdges.set(i, modifiedBinEdge);
+                }
+            }
+
+            // For each bin with idx=binEdges.size()-rest,...,binEdges.size()-1, widen the bin by one integer mass:
+            for(int i = 0; i < rest; i++){
+                int idx = binEdges.size() - rest + i;
+                int modifiedBinEdge = binEdges.get(idx) + i + 1;
+                binEdges.set(idx, modifiedBinEdge);
+            }
+
         }
         int[] binEdgesArray = new int[binEdges.size()];
         for(int i = 0; i < binEdges.size(); i++) binEdgesArray[i] = binEdges.get(i);
