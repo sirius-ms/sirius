7954e08b7083c3cb89e1ada37e6a9baf89580bee
mel <martin.engler-lukajewski@bright-giant.com>
2023-09-26 13:15:29 +0200

    added javadoc

--- db-storage/src/main/java/de/unijena/bioinf/storage/db/nosql/Filter.java
+++ db-storage/src/main/java/de/unijena/bioinf/storage/db/nosql/Filter.java
@@ -20,11 +20,23 @@
 
 package de.unijena.bioinf.storage.db.nosql;
 
+import org.dizitart.no2.NitriteId;
+import org.dizitart.no2.objects.ObjectRepository;
+
 import javax.validation.constraints.NotNull;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * A class to specify filtering criteria during {@link Database}'s
+ * find, count and remove operations.
+ * <p>
+ * Each filtering criteria is based on a field in the object.
+ * Please note that is the users responsibility to make sure that filter methods
+ * with Object value parameters are called with the same value type as the field type.
+ * Otherwise, the filtering might fail.
+ */
 public class Filter {
 
     public enum FilterType {
@@ -65,61 +77,123 @@ public class Filter {
 
     public Filter() {}
 
+    /**
+     * Concatenate multiple filters using an and operation, for example: {@code Filter.build().and().eq(...).gt(...).in(...)end()}.
+     * To avoid confusion, the end should be marked with {@link #end()}.
+     *
+     * @return Filter object.
+     */
     public Filter and() {
         this.filterChain.addLast(new FilterElement(FilterType.AND));
         return this;
     }
 
+    /**
+     * Concatenate multiple filters using an or operation, for example: {@code Filter.build().or().eq(...).gt(...).in(...)end()}.
+     * To avoid confusion, the end should be marked with {@link #end()}.
+     *
+     * @return Filter object.
+     */
     public Filter or() {
         this.filterChain.addLast(new FilterElement(FilterType.OR));
         return this;
     }
 
+    /**
+     * Mark the end of multiple filters concatenated by {@link #and()} or {@link #or()}.
+     *
+     * @return Filter object.
+     */
     public Filter end() {
         this.filterChain.addLast(new FilterElement(FilterType.END));
         return this;
     }
 
+    /**
+     * Negate the following filter.
+     *
+     * @return Filter object.
+     */
     public Filter not() {
         this.filterChain.addLast(new FilterElement(FilterType.NOT));
         return this;
     }
 
+    /**
+     * Equality filter. Please make sure that value is of the same type as the field type.
+     *
+     * @return Filter object.
+     */
     public Filter eq(String field, Object value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.EQ, field, value));
         return this;
     }
 
+    /**
+     * Greater than filter. Please make sure that value is of the same type as the field type.
+     *
+     * @return Filter object.
+     */
     public Filter gt(String field, Object value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.GT, field, value));
         return this;
     }
 
+    /**
+     * Greater than equals filter. Please make sure that value is of the same type as the field type.
+     *
+     * @return Filter object.
+     */
     public Filter gte(String field, Object value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.GTE, field, value));
         return this;
     }
 
+    /**
+     * Less then filter. Please make sure that value is of the same type as the field type.
+     *
+     * @return Filter object.
+     */
     public Filter lt(String field, Object value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.LT, field, value));
         return this;
     }
 
+    /**
+     * Less than equals filter. Please make sure that value is of the same type as the field type.
+     *
+     * @return Filter object.
+     */
     public Filter lte(String field, Object value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.LTE, field, value));
         return this;
     }
 
+    /**
+     * Full text search filter.
+     *
+     * @return Filter object.
+     */
     public Filter text(String field, String value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.TEXT, field, value));
         return this;
     }
 
+    /**
+     * Regex search filter.
+     *
+     * @return Filter object.
+     */
     public Filter regex(String field, String value) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.REGEX, field, value));
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inByte(String field, byte... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -129,6 +203,11 @@ public class Filter {
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inShort(String field, short... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -138,16 +217,31 @@ public class Filter {
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inInt(String field, int... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inLong(String field, long... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inFloat(String field, float... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -157,11 +251,21 @@ public class Filter {
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inDouble(String field, double... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inBool(String field, boolean... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -171,6 +275,11 @@ public class Filter {
         return this;
     }
 
+    /**
+     * In filter, field value may be one of the given values.
+     *
+     * @return Filter object.
+     */
     public Filter inChar(String field, char... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -180,11 +289,21 @@ public class Filter {
         return this;
     }
 
+    /**
+     * In filter, field may be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter in(String field, Object... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.IN, field, values));
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInByte(String field, byte... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -194,6 +313,11 @@ public class Filter {
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInShort(String field, short... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -203,16 +327,31 @@ public class Filter {
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInInt(String field, int... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.NOT_IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInLong(String field, long... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.NOT_IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInFloat(String field, float... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -222,11 +361,21 @@ public class Filter {
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInDouble(String field, double... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.NOT_IN, field, Arrays.stream(values).boxed().toArray()));
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInBool(String field, boolean... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -236,6 +385,11 @@ public class Filter {
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notInChar(String field, char... values) {
         Object[] objects = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -245,11 +399,24 @@ public class Filter {
         return this;
     }
 
+    /**
+     * Negated in filter, field value must not be one of the values.
+     *
+     * @return Filter object.
+     */
     public Filter notIn(String field, Object... values) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.NOT_IN, field, values));
         return this;
     }
 
+    /**
+     * Array and list matching filter. The following filter will be applied to all entries in the field.
+     * The array/list elements must be referenced using "$".
+     * <p>
+     * Example usage: {@code Filter.build().elemMatch("arrField").eq("$", value) }
+     *
+     * @return Filter object
+     */
     public Filter elemMatch(String field) {
         this.filterChain.addLast(new FieldFilterElement(FilterType.ELEM_MATCH, field));
         return this;
