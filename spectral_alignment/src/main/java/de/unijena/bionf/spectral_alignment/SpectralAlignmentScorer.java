0611870459e99e4adca6c50b95d76f6875bcc346
Andrii Kovalov <andrii.kovalov@bright-giant.com>
2023-10-04 11:25:36 +0200

    Moved code from AbstractSpectralAlignment to corresponding subclasses

--- spectral_alignment/src/main/java/de/unijena/bionf/spectral_alignment/SpectralAlignmentScorer.java
+++ spectral_alignment/src/main/java/de/unijena/bionf/spectral_alignment/SpectralAlignmentScorer.java
@@ -7,6 +7,7 @@ import de.unijena.bioinf.ChemistryBase.ms.utils.OrderedSpectrum;
 public interface SpectralAlignmentScorer {
 
     /**
+     * @param deviation should be higher than usual expected mass deviation to not punish mz errors too much. this results in low cosine scores even for the same compounds
      * @return similarity score of the two passed spectra
      */
     SpectralSimilarity score(OrderedSpectrum<Peak> left, OrderedSpectrum<Peak> right, double precursorLeft, double precursorRight, Deviation deviation);
