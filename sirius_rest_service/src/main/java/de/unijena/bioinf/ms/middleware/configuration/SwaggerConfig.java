6fafc8b7e5df21ae95a64d6ab65e3a978738909a
fleisch <markus.fleischauer@uni-jena.de>
2023-10-31 13:12:38 +0100

    fix description

--- sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/configuration/SwaggerConfig.java
+++ sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/configuration/SwaggerConfig.java
@@ -49,7 +49,7 @@ public class SwaggerConfig {
                 .description(
                         "REST API that provides the full functionality of SIRIUS and its web services as background service. " +
                         "It is intended as entry-point for scripting languages and software integration SDKs." +
-                        "This API is exposed by SIRIUS v" + ApplicationCore.VERSION_STRING()
+                        "This API is exposed by " + ApplicationCore.VERSION_STRING()
                 )
 //                .termsOfServiceUrl("https://bio.informatik.uni-jena.de/software/sirius/")
 //                .license("GNU General Public License v3.0")
