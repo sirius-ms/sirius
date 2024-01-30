5c9336fac9aedae7aa6ce8ee97f5e2578042de5b
marcusL <m.ludwig@uni-jena.de>
2024-01-30 13:48:21 +0100

    rename confidence score in StructureSearchResult for improved clarity

--- web_service_oss/sirius_web_client/src/main/java/de/unijena/bioinf/fingerid/StructureSearchResult.java
+++ web_service_oss/sirius_web_client/src/main/java/de/unijena/bioinf/fingerid/StructureSearchResult.java
@@ -9,16 +9,16 @@ import lombok.Getter;
 @Builder
 public class StructureSearchResult implements ResultAnnotation {
 
-    private final double score;
-    private final double scoreApproximate;
+    private final double confidenceScore;
+    private final double confidencScoreApproximate;
     private final int mcesIndex;
     private final ExpansiveSearchConfidenceMode.Mode expansiveSearchConfidenceMode;
 
 
     public static StructureSearchResult of(ConfidenceResult confidenceResult, ExpansiveSearchConfidenceMode.Mode expansiveSearchConfidenceMode) {
         return StructureSearchResult.builder()
-                .score(confidenceResult.score.score())
-                .scoreApproximate(confidenceResult.scoreApproximate.score())
+                .confidenceScore(confidenceResult.score.score())
+                .confidencScoreApproximate(confidenceResult.scoreApproximate.score())
                 .mcesIndex(confidenceResult.mcesIndex)
                 .expansiveSearchConfidenceMode(expansiveSearchConfidenceMode)
                 .build();
