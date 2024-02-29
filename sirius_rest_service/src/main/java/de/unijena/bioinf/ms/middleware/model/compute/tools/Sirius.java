99c8abe608a5cb874bc49de0c1e0c81a94653836
marcusL <m.ludwig@uni-jena.de>
2024-02-29 10:21:30 +0100

    parameter suggestions for Sirius.java API

--- sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/model/compute/tools/Sirius.java
+++ sirius_rest_service/src/main/java/de/unijena/bioinf/ms/middleware/model/compute/tools/Sirius.java
@@ -27,9 +27,7 @@ import de.unijena.bioinf.ChemistryBase.chem.Element;
 import de.unijena.bioinf.ChemistryBase.ms.MS2MassDeviation;
 import de.unijena.bioinf.ChemistryBase.ms.NumberOfCandidates;
 import de.unijena.bioinf.ChemistryBase.ms.NumberOfCandidatesPerIonization;
-import de.unijena.bioinf.ChemistryBase.ms.ft.model.FormulaSettings;
-import de.unijena.bioinf.ChemistryBase.ms.ft.model.IsotopeMs2Settings;
-import de.unijena.bioinf.ChemistryBase.ms.ft.model.Timeout;
+import de.unijena.bioinf.ChemistryBase.ms.ft.model.*;
 import de.unijena.bioinf.FragmentationTreeConstruction.model.UseHeuristic;
 import de.unijena.bioinf.ms.frontend.subtools.sirius.SiriusOptions;
 import de.unijena.bioinf.ms.properties.PropertyManager;
@@ -92,11 +90,45 @@ public class Sirius extends Tool<SiriusOptions> {
     IsotopeMs2Settings.Strategy isotopeMs2Settings;
 
     /**
+     * When filtering is enabled, molecular formulas are excluded if their theoretical isotope pattern does not match the theoretical one, even if their MS/MS pattern has high score.
+     */
+    Boolean filterByIsotopePattern;
+
+    ////////////////////////////////////////////////////////////////////////////
+    //// molecular formula generation and search ///////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * El Gordo may predict that an MS/MS spectrum is a lipid spectrum. If enabled, the corresponding molecular formula will be enforeced as molecular formula candidate.
+     */
+    Boolean enforceElGordoFormula;
+
+    /**
+     * If true, molecular formula generation via bottom up search is enabled.
+     */
+    Boolean performBottomUpSearch;
+
+    /**
+     * Specifies the m/z below which de novo molecular formula generation is enabled. Set to 0 to disable de novo molecular formula generation.
+     */
+    Double  performDenovoBelowMz;
+
+
+    /**
      * List Structure database to extract molecular formulas from to reduce formula search space.
      * SIRIUS is quite good at de novo formula annotation, so only enable if you have a good reason.
      */
     List<String> formulaSearchDBs;
 
+
+    /**
+     * By default, the formula (element) constraints are only applied to de novo molecular formula generation.
+     * If true, the constraints are as well applied to database search and bottom up search.
+     */
+    Boolean applyFormulaConstraintsToDBAndBottomUpSearch;
+
+    /////////////////////////////////////////////////////////////////////
+    //// formula constraints ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
     /**
      * These configurations hold the information how to autodetect elements based on the given formula constraints.
      * Note: If the compound is already assigned to a specific molecular formula, this annotation is ignored.
@@ -121,6 +153,9 @@ public class Sirius extends Tool<SiriusOptions> {
      */
     List<String> detectableElements;
 
+    ////////////////////////////////////////////////////////////////////////////
+    ///// fragmentation tree computation ///////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
     /**
      * Timout settings for the ILP solver used for fragmentation tree computation
      * secondsPerInstance: Set the maximum number of seconds for computing a single compound. Set to 0 to disable the time constraint.
@@ -134,6 +169,9 @@ public class Sirius extends Tool<SiriusOptions> {
      */
     UseHeuristic useHeuristic;
 
+    ////////////////////////////////////////////////////////////////////////////
+    ///// spectral library serach //////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
     /**
      * Similarity Threshold to inject formula candidates no matter which score/rank they have or which filter settings are applied.
      * If threshold >= 0 formulas candidates with reference spectrum similarity above the threshold will be injected.
@@ -161,15 +199,22 @@ public class Sirius extends Tool<SiriusOptions> {
                 .putNonNull("FormulaSettings.fallback", fallbackFormulaConstraints)
 
                 .putNonNull("IsotopeMs2Settings", isotopeMs2Settings)
+                .putNonNull("IsotopeSettings.filter", filterByIsotopePattern)
 
                 .putNonNull("MS2MassDeviation.allowedMassDeviation", massAccuracyMS2ppm, it -> it + " ppm")
 
+                .putNonNull("FormulaSearchSettings.performBottomUpAboveMz", (performBottomUpSearch==null || performBottomUpSearch==false) ? String.valueOf(Double.POSITIVE_INFINITY) : "0")
+                .putNonNull("FormulaSearchSettings.performDeNovoBelowMz", (performDenovoBelowMz==null) ? "0" : String.valueOf(performDenovoBelowMz))
                 .putNonNull("FormulaSearchDB", formulaSearchDBs, f -> String.join(",", f))
+
+                .putNonNull("FormulaSearchSettings.applyFormulaConstraintsToBottomUp", applyFormulaConstraintsToDBAndBottomUpSearch)
+                .putNonNull("FormulaSearchSettings.applyFormulaConstraintsToDatabaseCandidates", applyFormulaConstraintsToDBAndBottomUpSearch)
 
                 .putNonNull("NumberOfCandidates", numberOfCandidates)
                 .putNonNull("NumberOfCandidatesPerIonization", numberOfCandidatesPerIonization)
                 .putNonNull("AlgorithmProfile", profile)
 
+                .putNonNull("EnforceElGordoFormula", enforceElGordoFormula)
                 .putNonNull("InjectSpectralLibraryMatchFormulas.minScoreToInject", minScoreToInjectSpecLibMatch)
                 .putNonNullObj("InjectSpectralLibraryMatchFormulas.injectFormulas", minScoreToInjectSpecLibMatch, Objects::nonNull)
                 .toUnmodifiableMap();
@@ -185,7 +230,11 @@ public class Sirius extends Tool<SiriusOptions> {
                 .numberOfCandidatesPerIonization(PropertyManager.DEFAULTS.createInstanceWithDefaults(NumberOfCandidatesPerIonization.class).value)
                 .massAccuracyMS2ppm(PropertyManager.DEFAULTS.createInstanceWithDefaults(MS2MassDeviation.class).allowedMassDeviation.getPpm())
                 .isotopeMs2Settings(PropertyManager.DEFAULTS.createInstanceWithDefaults(IsotopeMs2Settings.class).value)
+                .filterByIsotopePattern(PropertyManager.DEFAULTS.createInstanceWithDefaults(IsotopeSettings.class).isFiltering())
+                .performBottomUpSearch(true)
+                .performDenovoBelowMz(PropertyManager.DEFAULTS.createInstanceWithDefaults(FormulaSearchSettings.class).performDeNovoBelowMz)
                 .formulaSearchDBs(List.of())
+                .applyFormulaConstraintsToDBAndBottomUpSearch(false) //todo not 100% sure about that since in principle API allows to do all 3 at once, database + bottom up + denovo
                 .enforcedFormulaConstraints(PropertyManager.DEFAULTS.createInstanceWithDefaults(FormulaSettings.class).getEnforcedAlphabet().toString())
                 .fallbackFormulaConstraints(PropertyManager.DEFAULTS.createInstanceWithDefaults(FormulaSettings.class).getFallbackAlphabet().toString())
                 .detectableElements(PropertyManager.DEFAULTS.createInstanceWithDefaults(FormulaSettings.class).getAutoDetectionElements().stream().map(Element::getSymbol).collect(Collectors.toList()))
