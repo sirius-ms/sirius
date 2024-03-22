072fa31f1bbcc9274ec22cfc81e23794675c6b30
fleisch <markus.fleischauer@uni-jena.de>
2024-03-22 13:13:39 +0100

    check supported file types in custom db panel.

--- sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/fingerid/custom_db/DatabaseImportConfigPanel.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/fingerid/custom_db/DatabaseImportConfigPanel.java
@@ -1,6 +1,7 @@
 package de.unijena.bioinf.ms.gui.fingerid.custom_db;
 
 import de.unijena.bioinf.ChemistryBase.utils.FileUtils;
+import de.unijena.bioinf.babelms.MsExperimentParser;
 import de.unijena.bioinf.ms.frontend.core.SiriusProperties;
 import de.unijena.bioinf.ms.frontend.io.FileChooserPanel;
 import de.unijena.bioinf.ms.frontend.subtools.custom_db.CustomDBOptions;
@@ -8,6 +9,7 @@ import de.unijena.bioinf.ms.gui.SiriusGui;
 import de.unijena.bioinf.ms.gui.compute.SubToolConfigPanel;
 import de.unijena.bioinf.ms.gui.compute.jjobs.Jobs;
 import de.unijena.bioinf.ms.gui.configs.Buttons;
+import de.unijena.bioinf.ms.gui.dialogs.InfoDialog;
 import de.unijena.bioinf.ms.gui.dialogs.input.DragAndDrop;
 import de.unijena.bioinf.ms.gui.net.ConnectionMonitor;
 import de.unijena.bioinf.ms.gui.utils.*;
@@ -19,6 +21,8 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
@@ -28,6 +32,7 @@ import java.awt.event.FocusEvent;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static de.unijena.bioinf.chemdb.custom.CustomDatabases.NOSQL_SUFFIX;
@@ -41,6 +46,38 @@ public class DatabaseImportConfigPanel extends SubToolConfigPanel<CustomDBOption
     private PlaceholderTextField dbNameField;
     private FileChooserPanel dbLocationField;
     private DefaultListModel<File> fileListModel;
+
+
+    private FileFilter supportedStructureFiles = new FileNameExtensionFilter("Structure files (e.g. .tsv, .sdf)", "tsv", "csv", "sdf");
+
+    private FileFilter supportedSpectraFiles = new FileFilter() {
+        @Override
+        public boolean accept(File f) {
+            String filename = f.getName().toLowerCase();
+            if (filename.endsWith(".mzml") || filename.endsWith(".mzxml"))
+                return false;
+            return MsExperimentParser.isSupportedFileName(filename);
+        }
+
+        @Override
+        public String getDescription() {
+            return "Spectra files (e.g. .msp, .json, .mgf, mat, .ms)";
+        }
+    };
+
+    private FileFilter supportedFiles = new FileFilter() {
+        @Override
+        public boolean accept(File f) {
+            return supportedStructureFiles.accept(f) || supportedSpectraFiles.accept(f);
+        }
+
+        @Override
+        public String getDescription() {
+            return "Structure and spectra files (e.g. .tsv, .sdf, .msp, .json, .mgf, mat, .ms)";
+        }
+    };
+
+
     public JButton importButton;
 
     private boolean validDbDisplayName;
@@ -77,9 +114,9 @@ public class DatabaseImportConfigPanel extends SubToolConfigPanel<CustomDBOption
     }
 
 
-    private boolean checkName(String name){
+    private boolean checkName(String name) {
         final String n = !name.endsWith(NOSQL_SUFFIX) ? (name + NOSQL_SUFFIX) : name;
-        return gui.applySiriusClient((c,pid) -> c.databases().getDatabaseWithResponseSpec(n,false)
+        return gui.applySiriusClient((c, pid) -> c.databases().getDatabaseWithResponseSpec(n, false)
                 .bodyToMono(SearchableDatabase.class).onErrorComplete().blockOptional().isPresent());
     }
 
@@ -249,6 +286,10 @@ public class DatabaseImportConfigPanel extends SubToolConfigPanel<CustomDBOption
 
         JFileChooser importFileChooser = new JFileChooser();
         importFileChooser.setMultiSelectionEnabled(true);
+        importFileChooser.addChoosableFileFilter(supportedFiles);
+        importFileChooser.addChoosableFileFilter(supportedSpectraFiles);
+        importFileChooser.addChoosableFileFilter(supportedStructureFiles);
+        importFileChooser.setFileFilter(supportedFiles);
 
         addFiles.addActionListener(e -> {
             if (importFileChooser.showOpenDialog(DatabaseImportConfigPanel.this) == JFileChooser.APPROVE_OPTION) {
@@ -264,8 +305,14 @@ public class DatabaseImportConfigPanel extends SubToolConfigPanel<CustomDBOption
         DropTarget dropTarget = new DropTarget() {
             @Override
             public synchronized void drop(DropTargetDropEvent evt) {
-                fileListModel.addAll(DragAndDrop.getFileListFromDrop(gui.getMainFrame(), evt));
-                refreshImportButton();
+                List<File> sf = DragAndDrop.getFileListFromDrop(gui.getMainFrame(), evt)
+                        .stream().filter(supportedFiles::accept).toList();
+                if (sf.isEmpty())
+                    new InfoDialog(gui.getMainFrame(), "No supported files found in input. " + supportedFiles.getDescription() + " are supported.");
+                else {
+                    fileListModel.addAll(sf);
+                    refreshImportButton();
+                }
             }
         };
         setDropTarget(dropTarget);
