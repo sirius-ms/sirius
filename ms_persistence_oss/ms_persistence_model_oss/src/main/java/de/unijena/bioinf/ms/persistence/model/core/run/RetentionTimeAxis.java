20f31767445902b7daa92ea4bf36c34bbabbb7bd
kaidu <kai.duehrkop@uni-jena.de>
2024-05-21 12:03:22 +0200

    add rest api for LCMS traces and quantification

--- ms_persistence_oss/ms_persistence_model_oss/src/main/java/de/unijena/bioinf/ms/persistence/model/core/run/RetentionTimeAxis.java
+++ ms_persistence_oss/ms_persistence_model_oss/src/main/java/de/unijena/bioinf/ms/persistence/model/core/run/RetentionTimeAxis.java
@@ -5,7 +5,10 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 /**
- * Each LC/MS run maps an index to a scan number and a retention time
+ * Each LC/MS run maps an index to a scan number and a retention time.
+ *
+ * It also contains other fields related to projecting the run onto the merged run. I don't
+ * know a better name for this class, though.
  */
 @Getter
 @Setter
@@ -24,5 +27,18 @@ public class RetentionTimeAxis {
      * Estimated noise level per scan to compute a local SNR value.
      */
     private float[] noiseLevelPerScan;
+
+    /**
+     * The scaling used for normalizing intensities
+     */
+    private double normalizationFactor = 1d;
+
+    /**
+     * we do not store the recalibration function, because it might be any kind of function
+     * (although it will be linear or spline in most cases). Instead, we just store
+     * the mapping of the retention times. The recalibration function can be approximated from that.
+     * Also, we can restore the ScanPointInterpolator knowing the recalibrated retention times!
+     */
+    private double[] recalbratedRetentionTimes;
 
 }
