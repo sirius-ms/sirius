0d785e1d02757951556e24461cdbbd0abc148a60
fleisch <markus.fleischauer@uni-jena.de>
2024-10-18 17:26:15 +0200

    use action directly instead of wrapping it in new action.

--- sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/mainframe/instance_panel/CompoundListView.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/mainframe/instance_panel/CompoundListView.java
@@ -24,6 +24,7 @@ import de.unijena.bioinf.ms.gui.SiriusGui;
 import de.unijena.bioinf.ms.gui.actions.SiriusActions;
 import de.unijena.bioinf.ms.gui.utils.JListDropImage;
 import de.unijena.bioinf.projectspace.InstanceBean;
+import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.event.*;
@@ -35,6 +36,7 @@ public class CompoundListView extends JScrollPane {
 
     final CompoundList sourceList;
     final JListDropImage<InstanceBean> compoundListView;
+    @Getter
     final JPopupMenu expPopMenu;
 
     public CompoundListView(SiriusGui gui, CompoundList sourceList) {
@@ -101,38 +103,17 @@ public class CompoundListView extends JScrollPane {
         KeyStroke enterKey = KeyStroke.getKeyStroke("ENTER");
         compoundListView.getInputMap().put(enterKey, SiriusActions.COMPUTE.name());
 
-
         KeyStroke delKey = KeyStroke.getKeyStroke("DELETE");
         compoundListView.getInputMap().put(delKey, SiriusActions.DELETE_EXP.name());
 
-
         ActionMap actionMap = compoundListView.getActionMap();
-
         // Define and register the compute action (for Enter key)
-        actionMap.put(SiriusActions.COMPUTE.name(), new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SiriusActions.COMPUTE.getInstance(gui, true)
-                        .actionPerformed(new ActionEvent(compoundListView, ActionEvent.ACTION_PERFORMED, SiriusActions.COMPUTE.name()));
-            }
-        });
-
+        actionMap.put(SiriusActions.COMPUTE.name(), SiriusActions.COMPUTE.getInstance(gui, true));
         // Define and register the delete action (for Delete key)
-        actionMap.put(SiriusActions.DELETE_EXP.name(), new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SiriusActions.DELETE_EXP.getInstance(gui, true)
-                        .actionPerformed(new ActionEvent(compoundListView, ActionEvent.ACTION_PERFORMED, SiriusActions.DELETE_EXP.name()));
-            }
-        });
+        actionMap.put(SiriusActions.DELETE_EXP.name(), SiriusActions.DELETE_EXP.getInstance(gui, true));
     }
 
     public void ensureIndexIsVisible(int index) {
         compoundListView.ensureIndexIsVisible(index);
     }
-
-    public JPopupMenu getExpPopMenu() {
-        return expPopMenu;
-    }
-
 }
