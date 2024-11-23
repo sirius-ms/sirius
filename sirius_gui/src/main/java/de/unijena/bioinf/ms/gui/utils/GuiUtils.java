d017a7fa345a1d81a2153cc53708f0f3c7b1139b
fleisch <markus.fleischauer@uni-jena.de>
2024-11-23 12:19:52 +0100

    minified svg icons, added new icons for landing Page, removed unused/replaced icons

--- sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/utils/GuiUtils.java
+++ sirius_gui/src/main/java/de/unijena/bioinf/ms/gui/utils/GuiUtils.java
@@ -23,13 +23,11 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
 import de.unijena.bioinf.ChemistryBase.utils.DescriptiveOptions;
-import de.unijena.bioinf.ChemistryBase.utils.FileUtils;
 import de.unijena.bioinf.ms.gui.configs.Colors;
 import de.unijena.bioinf.ms.gui.configs.Fonts;
 import de.unijena.bioinf.ms.gui.configs.Icons;
 import de.unijena.bioinf.ms.gui.dialogs.ExceptionDialog;
 import de.unijena.bioinf.ms.gui.webView.WebViewBrowserDialog;
-import de.unijena.bioinf.ms.gui.webView.WebViewJPanel;
 import de.unijena.bioinf.ms.properties.PropertyManager;
 import it.unimi.dsi.fastutil.Pair;
 import org.jetbrains.annotations.NotNull;
@@ -38,14 +36,11 @@ import org.slf4j.LoggerFactory;
 import raven.swing.spinner.SpinnerProgress;
 
 import javax.swing.*;
-import javax.swing.plaf.nimbus.AbstractRegionPainter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
@@ -102,60 +97,6 @@ public class GuiUtils {
         int offset = g2.getFontMetrics().stringWidth(icon);
         g2.drawString(icon, c.getWidth() - offset - 10, c.getHeight() - 8);
     }
-
-    public static class SimplePainter extends AbstractRegionPainter {
-
-        private Color fillColor;
-
-        public SimplePainter(Color color) {
-            // as a slight visual improvement, make the color transparent
-            // to at least see the background gradient
-            // the default progressBarPainter does it as well (plus a bit more)
-            fillColor = new Color(
-                    color.getRed(), color.getGreen(), color.getBlue(), 156);
-        }
-
-        @Override
-        protected void doPaint(Graphics2D g, JComponent c, int width,
-                               int height, Object[] extendedCacheKeys) {
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g.setColor(fillColor);
-            g.fillRect(0, 0, width, height);
-        }
-
-        @Override
-        protected PaintContext getPaintContext() {
-            return null;
-        }
-
-    }
-
-    public static class ProgressPainter implements Painter {
-
-        private Color light, dark;
-        private GradientPaint gradPaint;
-
-        public ProgressPainter(Color light, Color dark) {
-            this.light = light;
-            this.dark = dark;
-        }
-
-        @Override
-        public void paint(Graphics2D g, Object c, int w, int h) {
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            gradPaint = new GradientPaint((w / 2.0f), 0, light, (w / 2.0f), (h / 2.0f), dark, true);
-            g.setPaint(gradPaint);
-            g.fillRect(2, 2, (w - 5), (h - 5));
-
-            Color outline = new Color(0, 85, 0);
-            g.setColor(outline);
-            g.drawRect(2, 2, (w - 5), (h - 5));
-            Color trans = new Color(outline.getRed(), outline.getGreen(), outline.getBlue(), 100);
-            g.setColor(trans);
-            g.drawRect(1, 1, (w - 3), (h - 3));
-        }
-    }
-
 
     public static boolean assignParameterToolTip(@NotNull final JComponent comp, @NotNull String parameterKey) {
         final String parameterKeyShort = PropertyManager.DEFAULTS.shortKey(parameterKey);
@@ -248,6 +189,14 @@ public class GuiUtils {
         return Pair.of(p, label);
     }
 
+    public static void openURLOrError(@NotNull Frame owner, URI url) {
+        try {
+            openURL(owner, url, true);
+        } catch (IOException e) {
+            new ExceptionDialog(owner, "Error opening URL '" + url + "'. Cause: " + e.getMessage());
+        }
+    }
+
     public static void openURL(@NotNull Frame owner, URI url) throws IOException {
         openURL(owner, url, true);
     }
@@ -289,15 +238,6 @@ public class GuiUtils {
         });
     }
 
-    public static int getComponentIndex(JComponent parent, JComponent child) {
-        for (int i = 0; i < parent.getComponentCount(); ++i) {
-            if (parent.getComponent(i).equals(child)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     public static <T extends DescriptiveOptions> JComboBox<T> makeParameterComboBoxFromDescriptiveValues(T[] options) {
         return makeParameterComboBoxFromDescriptiveValues(options, null);
     }
@@ -324,28 +264,6 @@ public class GuiUtils {
         return box;
     }
 
-    public static JPanel newLoadingPanel() {
-        return newLoadingPanel("Loading...");
-    }
-
-    public static JPanel newLoadingPanel(@Nullable String loadingMessage) {
-        return newLoadingPanel(Icons.FP_LOADER, loadingMessage);
-    }
-
-    public static JPanel newLoadingPanel(@NotNull ImageIcon filterAnimation, @Nullable String loadingMessage) {
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setBackground(Colors.BACKGROUND);
-        //todo transparency would be cool
-        panel.setOpaque(true);
-        JLabel iconLabel = new JLabel(filterAnimation, SwingUtilities.CENTER);
-        JLabel label = loadingMessage != null && !loadingMessage.isBlank() ? new JLabel(loadingMessage) : null;
-
-        panel.add(iconLabel, BorderLayout.CENTER);
-        if (label != null)
-            panel.add(label, BorderLayout.SOUTH);
-        return panel;
-    }
-
     public static JPanel newSpinnerProgressPanel() {
         return newSpinnerProgressPanel("Loading...");
     }
@@ -356,38 +274,26 @@ public class GuiUtils {
 
     public static JPanel newSpinnerProgressPanel(@Nullable FlatSVGIcon filterIcon, @Nullable String loadingMessage) {
         JPanel panel = new JPanel(new BorderLayout());
-        panel.setBackground(Colors.BACKGROUND);
+        panel.setOpaque(false);
 
-        panel.setOpaque(true);
         SpinnerProgress spinner = filterIcon == null ? new SpinnerProgress() : new SpinnerProgress(filterIcon);
         spinner.setStringPainted(false);
         spinner.setIndeterminate(true);
         spinner.setPreferredSize(new Dimension(128, 128));
+        spinner.setOpaque(false);
 
         JLabel label = loadingMessage != null && !loadingMessage.isBlank() ? new JLabel(loadingMessage) : null;
+        if (label != null)
+            label.setOpaque(false);
 
         // Create a wrapper panel to hold the fixed-size panel
         JPanel wrapperPanel = new JPanel(new GridBagLayout());
         wrapperPanel.add(spinner); // Add fixed-size panel to the center of the wrapper
+        wrapperPanel.setOpaque(false);
 
         panel.add(wrapperPanel, BorderLayout.CENTER);
         if (label != null)
             panel.add(label, BorderLayout.SOUTH);
         return panel;
     }
-
-    public static WebViewJPanel newLoadingWebPanel() {
-        final WebViewJPanel panel = new WebViewJPanel();
-        final StringBuilder buf = new StringBuilder();
-        try (final BufferedReader br = FileUtils.ensureBuffering(new InputStreamReader(GuiUtils.class.getResourceAsStream("/animations/eclipse-loader-160px.html")))) {
-            String line;
-            while ((line = br.readLine()) != null) buf.append(line).append('\n');
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        panel.load(buf.toString());
-        return panel;
-    }
-
-
 }
