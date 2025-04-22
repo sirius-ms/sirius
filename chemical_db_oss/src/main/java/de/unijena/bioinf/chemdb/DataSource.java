82410bb9ad0d16d0844aba3e7fe5b2e1489845b6
Andrii Kovalov <andrii.kovalov@bright-giant.com>
2025-04-22 18:43:38 +0200

    Change binary literals to bitshift in DataSource
    Change Publication from class to record

--- chemical_db_oss/src/main/java/de/unijena/bioinf/chemdb/DataSource.java
+++ chemical_db_oss/src/main/java/de/unijena/bioinf/chemdb/DataSource.java
@@ -33,37 +33,37 @@ public enum DataSource {
     ALL("All included DBs", makeALLFLAG(), null, null, null, null),
     //the BIO flag is a collection of many bio-like databases. Furthermore, there was a flag 128 in the PSQL structure database which was called bio. This is now obsolete and replaced by a combined flags.
     BIO("Bio Database", makeBIOFLAG(), null, null, null, null), //todo make distinction to normal databases more clear
-    PUBCHEM("PubChem", 0b10, "compound_id","pubchem", "https://pubchem.ncbi.nlm.nih.gov/compound/%s", new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
-    MESH("MeSH", 0b100, "compound_id", "hasmesh", null, null),
-    HMDB("HMDB", 0b1000, "hmdb_id", "hmdb", "http://www.hmdb.ca/metabolites/%s", new Publication("Wishart DS, Guo AC, Oler E, et al., HMDB 5.0: the Human Metabolome Database for 2022. Nucleic Acids Res. 2022", "10.1093/nar/gkab1062")),
-    KNAPSACK("KNApSAcK", 0b10000, "inchi_key", "knapsack", "http://www.knapsackfamily.com/knapsack_core/result.php?sname=INCHIKEY&word=%s", new Publication("Nakamura Y. et al, KNApSAcK Metabolite Activity Database for retrieving the relationships between metabolites and biological activities. Plant Cell Physiol.  2014", "10.1093/pcp/pct176")),
-    CHEBI("CHEBI", 0b100000, "chebi_id","chebi", "https://www.ebi.ac.uk/chebi/searchId.do?chebiId=%s", new Publication("Hastings J et al., ChEBI in 2016: Improved services and an expanding collection of metabolites. Nucleic Acids Res. 2016", "10.1093/nar/gkv1031")),
-    PUBMED("PubMed", 0b1000000, null, null, null, null),
-    KEGG("KEGG", 0b100000000, "kegg_id","kegg", "http://www.kegg.jp/dbget-bin/www_bget?cpd:%s", new Publication("Kanehisa M and Goto S, KEGG: Kyoto Encyclopedia of Genes and Genomes. Nucleic Acids Res. 2000.", "10.1093/nar/28.1.27")),
-    HSDB("HSDB", 0b1000000000, "cas","hsdb", null, new Publication("Fonger GC et al., The National Library of Medicine's (NLM) Hazardous Substances Data Bank (HSDB): background, recent enhancements and future plans. Toxicology. 2014", "10.1016/j.tox.2014.09.003")),
-    MACONDA("Maconda", 0b10000000000, "maconda_id","maconda", "http://www.maconda.bham.ac.uk/contaminant.php?id=%d", new Publication("Weber RJM et al., MaConDa: a publicly accessible mass spectrometry contaminants database. Bioinformatics. 2012", "10.1093/bioinformatics/bts527")),
-    METACYC("Biocyc", 0b100000000000, "unique_id","biocyc", "http://biocyc.org/compound?orgid=META&id=%s", new Publication("Caspi R et al., The MetaCyc database of metabolic pathways and enzymes - a 2019 update, Nucleic Acids Res, 2020", "10.1093/nar/gkz862")),
-    GNPS("GNPS", 0b1000000000000, "id","gnps", "https://gnps.ucsd.edu/ProteoSAFe/gnpslibraryspectrum.jsp?SpectrumID=%s", null), //todo this should be part only of a spectral library search tool.
-    TRAIN("Training Set", 0b100000000000000, null, null, null, null), //not part of the PSQL database anymore but assigned for each predictor individually //todo but we still need that flag, right?
-    YMDB("YMDB", 0b10000000000000000, "ymdb_id","ymdb", "http://www.ymdb.ca/compounds/%s", new Publication("Ramirez-Gaona M et al., YMDB 2.0: a significantly expanded version of the yeast metabolome database. Nucleic Acids Res. 2017", "10.1093/nar/gkw1058")),
-    PLANTCYC("Plantcyc", 0b100000000000000000, "compound_id","plantcyc",  "http://pmn.plantcyc.org/compound?orgid=PLANT&id=%s", new Publication("Hawkins C et al., Plant Metabolic Network 15: A resource of genome-wide metabolism databases for 126 plants and algae. J Integr Plant Biol. 2021", "10.1111/jipb.13163")),
-    NORMAN("NORMAN", 0b1000000000000000000, "norman_susdat_id","norman", null, new Publication("Taha HM et al., The NORMAN Suspect List Exchange (NORMAN-SLE): facilitating European and worldwide collaboration on suspect screening in high resolution mass spectrometry. Environ Sci Eur. 2022", "10.1186/s12302-022-00680-6")),
-    SUPERNATURAL("SuperNatural", 0b100000000000000000000, "id", "supernatural", "http://bioinf-applied.charite.de/supernatural_new/index.php?site=compound_search&start=0&supplier=all&tox=any&classification=all&compound_input=true&sn_id=%s", new Publication("Gallo K et al., SuperNatural 3.0-a database of natural products and natural product-based derivatives. Nucleic Acids Res. 2022.", "10.1093/nar/gkac1008")),
-    COCONUT("COCONUT", 0b1000000000000000000000, "id", "coconut", "https://coconut.naturalproducts.net/compound/coconut_id/%s", new Publication("Sorokina M et al., COCONUT online: Collection of Open Natural Products database. J Cheminf. 2021", "10.1186/s13321-020-00478-9")),
+    PUBCHEM("PubChem", 1<<1, "compound_id","pubchem", "https://pubchem.ncbi.nlm.nih.gov/compound/%s", new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
+    MESH("MeSH", 1<<2, "compound_id", "hasmesh", null, null),
+    HMDB("HMDB", 1<<3, "hmdb_id", "hmdb", "http://www.hmdb.ca/metabolites/%s", new Publication("Wishart DS, Guo AC, Oler E, et al., HMDB 5.0: the Human Metabolome Database for 2022. Nucleic Acids Res. 2022", "10.1093/nar/gkab1062")),
+    KNAPSACK("KNApSAcK", 1<<4, "inchi_key", "knapsack", "http://www.knapsackfamily.com/knapsack_core/result.php?sname=INCHIKEY&word=%s", new Publication("Nakamura Y. et al, KNApSAcK Metabolite Activity Database for retrieving the relationships between metabolites and biological activities. Plant Cell Physiol.  2014", "10.1093/pcp/pct176")),
+    CHEBI("CHEBI", 1<<5, "chebi_id","chebi", "https://www.ebi.ac.uk/chebi/searchId.do?chebiId=%s", new Publication("Hastings J et al., ChEBI in 2016: Improved services and an expanding collection of metabolites. Nucleic Acids Res. 2016", "10.1093/nar/gkv1031")),
+    PUBMED("PubMed", 1<<6, null, null, null, null),
+    KEGG("KEGG", 1<<8, "kegg_id","kegg", "http://www.kegg.jp/dbget-bin/www_bget?cpd:%s", new Publication("Kanehisa M and Goto S, KEGG: Kyoto Encyclopedia of Genes and Genomes. Nucleic Acids Res. 2000.", "10.1093/nar/28.1.27")),
+    HSDB("HSDB", 1<<9, "cas","hsdb", null, new Publication("Fonger GC et al., The National Library of Medicine's (NLM) Hazardous Substances Data Bank (HSDB): background, recent enhancements and future plans. Toxicology. 2014", "10.1016/j.tox.2014.09.003")),
+    MACONDA("Maconda", 1<<10, "maconda_id","maconda", "http://www.maconda.bham.ac.uk/contaminant.php?id=%d", new Publication("Weber RJM et al., MaConDa: a publicly accessible mass spectrometry contaminants database. Bioinformatics. 2012", "10.1093/bioinformatics/bts527")),
+    METACYC("Biocyc", 1<<11, "unique_id","biocyc", "http://biocyc.org/compound?orgid=META&id=%s", new Publication("Caspi R et al., The MetaCyc database of metabolic pathways and enzymes - a 2019 update, Nucleic Acids Res, 2020", "10.1093/nar/gkz862")),
+    GNPS("GNPS", 1<<12, "id","gnps", "https://gnps.ucsd.edu/ProteoSAFe/gnpslibraryspectrum.jsp?SpectrumID=%s", null), //todo this should be part only of a spectral library search tool.
+    TRAIN("Training Set", 1<<14, null, null, null, null), //not part of the PSQL database anymore but assigned for each predictor individually //todo but we still need that flag, right?
+    YMDB("YMDB", 1<<16, "ymdb_id","ymdb", "http://www.ymdb.ca/compounds/%s", new Publication("Ramirez-Gaona M et al., YMDB 2.0: a significantly expanded version of the yeast metabolome database. Nucleic Acids Res. 2017", "10.1093/nar/gkw1058")),
+    PLANTCYC("Plantcyc", 1<<17, "compound_id","plantcyc",  "http://pmn.plantcyc.org/compound?orgid=PLANT&id=%s", new Publication("Hawkins C et al., Plant Metabolic Network 15: A resource of genome-wide metabolism databases for 126 plants and algae. J Integr Plant Biol. 2021", "10.1111/jipb.13163")),
+    NORMAN("NORMAN", 1<<18, "norman_susdat_id","norman", null, new Publication("Taha HM et al., The NORMAN Suspect List Exchange (NORMAN-SLE): facilitating European and worldwide collaboration on suspect screening in high resolution mass spectrometry. Environ Sci Eur. 2022", "10.1186/s12302-022-00680-6")),
+    SUPERNATURAL("SuperNatural", 1<<20, "id", "supernatural", "http://bioinf-applied.charite.de/supernatural_new/index.php?site=compound_search&start=0&supplier=all&tox=any&classification=all&compound_input=true&sn_id=%s", new Publication("Gallo K et al., SuperNatural 3.0-a database of natural products and natural product-based derivatives. Nucleic Acids Res. 2022.", "10.1093/nar/gkac1008")),
+    COCONUT("COCONUT", 1<<21, "id", "coconut", "https://coconut.naturalproducts.net/compound/coconut_id/%s", new Publication("Sorokina M et al., COCONUT online: Collection of Open Natural Products database. J Cheminf. 2021", "10.1186/s13321-020-00478-9")),
 
-    BloodExposome("Blood Exposome", 0b10000000000000000000000, "pubchem_cid", "bloodexposome", "https://bloodexposome.org/#/description?qcid=%s", new Publication("Barupal DK and Fiehn O, Generating the Blood Exposome Database Using a Comprehensive Text Mining and Database Fusion Approach. Environ Health Perspect. 2019", "10.1289/EHP4713")),
-    TeroMol("TeroMOL", 0b100000000000000000000000, "mol_id", "teromol", "http://terokit.qmclab.com/molecule.html?MolId=%s", new Publication("Zeng T et al.,Chemotaxonomic Investigation of Plant Terpenoids with an Established Database (TeroMOL). New Phytol. 2022", "10.1111/nph.18133")),
+    BloodExposome("Blood Exposome", 1<<22, "pubchem_cid", "bloodexposome", "https://bloodexposome.org/#/description?qcid=%s", new Publication("Barupal DK and Fiehn O, Generating the Blood Exposome Database Using a Comprehensive Text Mining and Database Fusion Approach. Environ Health Perspect. 2019", "10.1289/EHP4713")),
+    TeroMol("TeroMOL", 1<<23, "mol_id", "teromol", "http://terokit.qmclab.com/molecule.html?MolId=%s", new Publication("Zeng T et al.,Chemotaxonomic Investigation of Plant Terpenoids with an Established Database (TeroMOL). New Phytol. 2022", "10.1111/nph.18133")),
 
-    PUBCHEMANNOTATIONBIO("PubChem: bio and metabolites", 0b1000000000000000000000000, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
-    PUBCHEMANNOTATIONDRUG("PubChem: drug", 0b10000000000000000000000000, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
-    PUBCHEMANNOTATIONSAFETYANDTOXIC("PubChem: safety and toxic", 0b100000000000000000000000000, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
-    PUBCHEMANNOTATIONFOOD("PubChem: food", 0b1000000000000000000000000000, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
+    PUBCHEMANNOTATIONBIO("PubChem: bio and metabolites", 1<<24, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
+    PUBCHEMANNOTATIONDRUG("PubChem: drug", 1<<25, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
+    PUBCHEMANNOTATIONSAFETYANDTOXIC("PubChem: safety and toxic", 1<<26, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
+    PUBCHEMANNOTATIONFOOD("PubChem: food", 1<<27, null, null, null, new Publication("Kim S et al., PubChem in 2021: new data content and improved web interfaces. Nucleic Acids Res. 2021", "10.1093/nar/gkaa971")),
 
-    LOTUS("LOTUS", 0b10000000000000000000000000000, "id", "lotus", "https://lotus.naturalproducts.net/search/simple/%s", new Publication("Rutz A et al., The LOTUS initiative for open knowledge management in natural products research. eLife. 2022", "10.7554/eLife.70780")),
-    FooDB("FooDB", 0b100000000000000000000000000000, "fooddb_id", "foodDB", "https://foodb.ca/compounds/%s", new Publication("www.foodb.ca", null)),
-    MiMeDB("MiMeDB", 0b1000000000000000000000000000000, "mimeDB_id", "mimeDB", "https://mimedb.org/metabolites/%s", new Publication("Wishart DS et al., MiMeDB: the Human Microbial Metabolome Database. Nucleic Acids Res. 2023", "10.1093/nar/gkac868")),
-    LIPIDMAPS("LipidMaps", 0b10000000000000000000000000000000L, "id", "lipidmaps", "https://www.lipidmaps.org/databases/lmsd/%s", new Publication("Sud M et al., LMSD: LIPID MAPS structure database. Nucleic Acids Res. 2006", "10.1093/nar/gkl838")),
-    LIPID("Lipid", 0b100000000000000000000000000000000L, null, null, "https://www.lipidmaps.org/databases/lmsd/%s", null),
+    LOTUS("LOTUS", 1<<28, "id", "lotus", "https://lotus.naturalproducts.net/search/simple/%s", new Publication("Rutz A et al., The LOTUS initiative for open knowledge management in natural products research. eLife. 2022", "10.7554/eLife.70780")),
+    FooDB("FooDB", 1<<29, "fooddb_id", "foodDB", "https://foodb.ca/compounds/%s", new Publication("www.foodb.ca", null)),
+    MiMeDB("MiMeDB", 1<<30, "mimeDB_id", "mimeDB", "https://mimedb.org/metabolites/%s", new Publication("Wishart DS et al., MiMeDB: the Human Microbial Metabolome Database. Nucleic Acids Res. 2023", "10.1093/nar/gkac868")),
+    LIPIDMAPS("LipidMaps", 1L<<31, "id", "lipidmaps", "https://www.lipidmaps.org/databases/lmsd/%s", new Publication("Sud M et al., LMSD: LIPID MAPS structure database. Nucleic Acids Res. 2006", "10.1093/nar/gkl838")),
+    LIPID("Lipid", 1L<<32, null, null, "https://www.lipidmaps.org/databases/lmsd/%s", null),
 /*"https://www.lipidmaps.org/rest/compound/abbrev/%s/all/txt"*/ //todo which is the correect query?
 
 
@@ -77,7 +77,7 @@ public enum DataSource {
     //////////////////////////////////////////
 
 
-    DSSTox("DSSTox", 0b1000000000000000000000000000000000000L, "dsstox_substance_id","dsstox", "https://comptox.epa.gov/dashboard/%s", new Publication("Grulke CM at al., EPA's DSSTox database: History of development of a curated chemistry resource supporting computational toxicology research. Comput Toxicol. 2019", "10.1016/j.comtox.2019.100096"));
+    DSSTox("DSSTox", 1L<<37, "dsstox_substance_id","dsstox", "https://comptox.epa.gov/dashboard/%s", new Publication("Grulke CM at al., EPA's DSSTox database: History of development of a curated chemistry resource supporting computational toxicology research. Comput Toxicol. 2019", "10.1016/j.comtox.2019.100096"));
 
     // additional field
     public final long flag;
@@ -97,7 +97,7 @@ public enum DataSource {
         this.publication = publication;
     }
 
-    private static Pattern NUMPAT = Pattern.compile("%[0-9 ,+\\-]*d");
+    private static final Pattern NUMPAT = Pattern.compile("%[0-9 ,+\\-]*d");
 
     public String getLink(String id) {
         if (this.URI == null) return null;
@@ -190,21 +190,5 @@ public enum DataSource {
     /**
      * to reference a publication for a specific data source
      */
-    public static class Publication {
-        final String citationText;
-        final String doi;
-
-        public Publication(String citationText, String doi) {
-            this.citationText = citationText;
-            this.doi = doi;
-        }
-
-        public String getCitationText() {
-            return citationText;
-        }
-
-        public String getDoi() {
-            return doi;
-        }
-    }
+    public record Publication(String citationText, String doi) {}
 }
