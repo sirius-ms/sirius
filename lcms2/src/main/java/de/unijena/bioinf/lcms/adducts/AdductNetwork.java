0c2d22285d93f9b9124520f31d9b084321ceaf02
kaidu <kai.duehrkop@bright-giant.com>
2025-05-23 12:41:26 +0200

    fix #706

--- lcms2/src/main/java/de/unijena/bioinf/lcms/adducts/AdductNetwork.java
+++ lcms2/src/main/java/de/unijena/bioinf/lcms/adducts/AdductNetwork.java
@@ -69,7 +69,6 @@ public class AdductNetwork {
         double voidVolumeStart = rtOrderedNodes[0].getRetentionTime();
         double voidVolumeEnd = voidVolumeStart + 4*retentionTimeTolerance;
         double endOfLc = rtOrderedNodes[rtOrderedNodes.length-1].getRetentionTime() - 4*retentionTimeTolerance;
-
         List<BasicJJob<NetworkResult>> jobs = new ArrayList<>();
         for (int rr=0; rr < rtOrderedNodes.length; ++rr) {
             final int r = rr;
@@ -130,7 +129,7 @@ public class AdductNetwork {
                                 if (!knownMassDeltas.isEmpty()) {
                                     final AdductEdge adductEdge = new AdductEdge(leftNode, rightNode, knownMassDeltas.toArray(KnownMassDelta[]::new));
                                     scorer.computeScore(provider, adductEdge);
-                                    if (Double.isFinite(adductEdge.extraSampleCorrelation)) {
+                                    if (adductEdge.isValid()) {
 
                                         // add MS/MS score
                                         if (!ms2Right.isEmpty()) {
@@ -154,7 +153,7 @@ public class AdductNetwork {
                                 } else if (rt.getStartTime() > voidVolumeEnd && rt.getEndTime() < endOfLc && decoyEdges.size() < 10 && adductManager.hasDecoy(massDelta)) {
                                     final AdductEdge adductEdge = new AdductEdge(leftNode, rightNode, new KnownMassDelta[0]);
                                     scorer.computeScore(provider, adductEdge);
-                                    if (Double.isFinite(adductEdge.extraSampleCorrelation)) {
+                                    if (adductEdge.isValid()) {
                                         decoyEdges.add(adductEdge);
                                     }
                                 }
@@ -194,7 +193,6 @@ public class AdductNetwork {
                 }
             }
         }
-
     }
 
     private MassMap<Peak> getPotentialInsourceFragments(List<MergedMSnSpectrum> data, AdductNode rightNode) {
@@ -807,11 +805,11 @@ public class AdductNetwork {
         }
 
         public void add(AdductEdge edge) {
-            merged.get(edge.interSampleCorrelationCount).add(edge.interSampleCorrelation);
-            representative.get(edge.interSampleCorrelationRepresentativeCount).add(edge.interSampleCorrelationRepresentative);
-            extra.get(edge.extraSampleCorrelationCount).add(edge.extraSampleCorrelation);
-            rt.add(edge.rtScore);
-            ms2.add(edge.ms2score);
+            if (Double.isFinite(edge.interSampleCorrelation)) merged.get(edge.interSampleCorrelationCount).add(edge.interSampleCorrelation);
+            if (Double.isFinite(edge.interSampleCorrelationRepresentative)) representative.get(edge.interSampleCorrelationRepresentativeCount).add(edge.interSampleCorrelationRepresentative);
+            if (Double.isFinite(edge.extraSampleCorrelation)) extra.get(edge.extraSampleCorrelationCount).add(edge.extraSampleCorrelation);
+            if (Double.isFinite(edge.rtScore)) rt.add(edge.rtScore);
+            if (Double.isFinite(edge.ms2score)) ms2.add(edge.ms2score);
         }
 
     }
