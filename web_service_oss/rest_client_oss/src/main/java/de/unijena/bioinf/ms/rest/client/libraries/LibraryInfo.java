e8e3c2d989636961c0490f05ac26a5b352afa5a4
Andrii Kovalov <andrii.kovalov@bright-giant.com>
2025-09-03 10:07:22 +0200

    Remove version from library info
    [skip ci]

--- web_service_oss/rest_client_oss/src/main/java/de/unijena/bioinf/ms/rest/client/libraries/LibraryInfo.java
+++ web_service_oss/rest_client_oss/src/main/java/de/unijena/bioinf/ms/rest/client/libraries/LibraryInfo.java
@@ -1,3 +1,3 @@
 package de.unijena.bioinf.ms.rest.client.libraries;
 
-public record LibraryInfo(String id, String version, String description, long size) { }
+public record LibraryInfo(String id, String description, long size) { }
