b53f0c8aa60ceb9c9cf9d454aa535c4dc904c6df
Andrii Kovalov <andrii.kovalov@bright-giant.com>
2025-10-01 09:57:05 +0200

    Add withLinks option to DB export

--- sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/subtools/custom_db/export/TsvExporter.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/subtools/custom_db/export/TsvExporter.java
@@ -1,21 +1,33 @@
 package de.unijena.bioinf.ms.frontend.subtools.custom_db.export;
 
+import de.unijena.bioinf.ChemistryBase.chem.InChIs;
 import de.unijena.bioinf.chemdb.CompoundCandidate;
 import de.unijena.bioinf.chemdb.nitrite.wrappers.FingerprintCandidateWrapper;
 import de.unijena.bioinf.ms.frontend.subtools.summaries.TsvTableWriter;
 import de.unijena.bioinf.ms.frontend.utils.DatabaseLinkUtil;
+import de.unijena.bioinf.webapi.WebAPI;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TsvExporter extends DbExporter {
 
     private final TsvTableWriter tsvWriter;
+    private final WebAPI<?> api;
+    private final boolean withLinks;
 
-    public TsvExporter(BufferedWriter writer) throws IOException {
+    public TsvExporter(BufferedWriter writer, WebAPI<?> api, boolean withLinks) throws IOException {
+        this.api = api;
+        this.withLinks = withLinks;
         tsvWriter = new TsvTableWriter(writer, false);
-        tsvWriter.writeHeader(List.of("name", "SMILES", "InChIkey2D", "InChI", "formula", "mass", "links"));
+        List<String> columns = new ArrayList<>(List.of("name", "SMILES", "InChIkey2D", "InChI", "formula", "mass"));
+        if (withLinks) {
+            columns.add("links");
+        }
+        tsvWriter.writeHeader(columns);
     }
 
     @Override
@@ -25,15 +37,34 @@ public class TsvExporter extends DbExporter {
 
     private List<Object> extractValues(FingerprintCandidateWrapper cw) throws IOException {
         CompoundCandidate c = cw.getCandidate(null, null);
-        return List.of(
+
+        List<Object> values = new ArrayList<>(List.of(
                 c.getName(),
                 c.getSmiles(),
                 c.getInchiKey2D(),
                 c.getInchi().in2D,
                 cw.getFormula(),
-                cw.getMass(),
-                DatabaseLinkUtil.links(c.getLinkedDatabases())
-        );
+                cw.getMass()));
+
+        if (withLinks) {
+            String otherDBLinks = api.applyStructureDB(0, db -> {
+                try {
+                    List<CompoundCandidate> formulaCandidates = db.lookupStructuresByFormula(InChIs.extractNeutralFormulaByAdjustingHsOrThrow(c.getInchi().in2D));
+                    for (CompoundCandidate can : formulaCandidates) {
+                        if (can.getInchiKey2D().equals(c.getInchiKey2D())) {
+                            Map<String, List<String>> links = can.getLinkedDatabases();
+                            return DatabaseLinkUtil.links(links);
+                        }
+                    }
+                    return "";
+                } catch (Exception e) {
+                    return "";
+                }
+            });
+            values.add(otherDBLinks);
+        }
+
+        return values;
     }
 
     @Override
